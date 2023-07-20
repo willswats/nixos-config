b5b81436c028c481e1f7e081730f34844eb61227
willswats <williamstuwatson@gmail.com>
2023-07-20 17:51:53 +0100

    Setup lightdm theme

--- configuration.nix
+++ configuration.nix
@@ -79,8 +79,37 @@
       };
 
       displayManager = {
+        lightdm = {
+          enable = true;
+          background = ./wallpapers/minimal-desert.png;
+          greeters.gtk = {
+            enable = true;
+            theme = {
+              name = "Catppuccin-Mocha-Standard-Blue-dark";
+              package = pkgs.catppuccin-gtk.override {
+                accents = [ "blue" ];
+                size = "standard";
+                variant = "mocha";
+              };
+            };
+            iconTheme = {
+              name = "Papirus-Dark";
+              package = pkgs.catppuccin-papirus-folders.override {
+                accent = "blue";
+                flavor = "mocha";
+              };
+            };
+            cursorTheme = {
+              name = "Catppuccin-Mocha-Dark-Cursors";
+              package = pkgs.catppuccin-cursors.mochaDark;
+              size = 32; # 24 32 48 64
+            };
+          };
+        };
         defaultSession = "none+i3";
       };
+
+
 
       windowManager.i3 = {
         enable = true;
