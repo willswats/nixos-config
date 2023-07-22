ab3b779dae28bf9779dd156b5c16c7226a6f0160
willswats <williamstuwatson@gmail.com>
2023-07-22 02:39:27 +0100

    Add comment

--- modules/rustup/default.nix
+++ modules/rustup/default.nix
@@ -1,6 +1,7 @@
 { ... }:
 
 {
+  # This removes the need for the command "rustup default stable" on fresh install
   home.file.".rustup/settings.toml".text = ''
     default_toolchain = "stable-x86_64-unknown-linux-gnu"
     profile = "default"
