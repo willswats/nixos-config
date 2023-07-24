4db5fb37b3534f5d1f860d3628ddf13c7080ba89
willswats <williamstuwatson@gmail.com>
2023-07-24 14:06:21 +0100

    Add xrandr to xserver setupCommands

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -19,5 +19,14 @@
   boot.initrd.luks.devices."luks-1bb26d75-1e56-449f-9a51-bb94ea8183e7".device = "/dev/disk/by-uuid/1bb26d75-1e56-449f-9a51-bb94ea8183e7";
   boot.initrd.luks.devices."luks-1bb26d75-1e56-449f-9a51-bb94ea8183e7".keyFile = "/crypto_keyfile.bin";
 
+  xserver.displayManager.setupCommands =
+    let
+      monitor-center = "DP-2";
+      monitor-left = "DP-3";
+    in
+    ''
+      ${pkgs.xorg.xrandr}/bin/xrandr --output ${monitor-center} --primary --mode 1920x1080 --rate 144.00 --rotate normal --output ${monitor-left} --mode 1920x1080 --rate 144.00 --rotate right --left-of ${monitor-center}
+    '';
+
   networking.hostName = "will-desktop";
 }
