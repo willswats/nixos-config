836657cb72770cbaaa8a8d9c68ec4a1a097783dc
willswats <williamstuwatson@gmail.com>
2023-07-26 14:51:00 +0100

    Remove space

--- modules/gnome-keyring/home.nix
+++ modules/gnome-keyring/home.nix
@@ -5,7 +5,6 @@
     gnome-keyring.enable = true;
   };
 
-
   xsession.profileExtra = ''
     eval $(${pkgs.gnome3.gnome-keyring}/bin/gnome-keyring-daemon --daemonize --components=ssh,secrets)
     export SSH_AUTH_SOCK
