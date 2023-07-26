e365e787b0abad15fdef7db07ba7e1f3f480674b
willswats <williamstuwatson@gmail.com>
2023-07-26 15:36:18 +0100

    Add pipewire

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -2,15 +2,13 @@
 
 {
   imports = [
+    ../modules/pipewire
     ../modules/theme
     ../modules/i3
     ../modules/plymouth
     ../modules/pcmanfm
-    ../modules/virt-manager
     ../modules/mullvad-vpn
   ];
-
-  networking.networkmanager.enable = true;
 
   time.timeZone = "Europe/London";
 
@@ -28,6 +26,8 @@
       LC_TIME = "en_GB.UTF-8";
     };
   };
+
+  console.keyMap = "uk";
 
   services.xserver = {
     enable = true;
@@ -47,9 +47,14 @@
     };
   };
 
+  # Networking
+  networking.networkmanager.enable = true;
+
+  # Enable polkit
   security.polkit.enable = true;
 
-  console.keyMap = "uk";
+  # Enable bluetooth 
+  hardware.bluetooth.enable = true;
 
   users.users.will = {
     isNormalUser = true;
@@ -60,13 +65,6 @@
 
   programs.fish.enable = true;
 
-  nixpkgs.config = {
-    # Allow unfree packages
-    allowUnfree = true;
-    # Explicit PulseAudio support in applications
-    pulseaudio = true;
-  };
-
   environment = {
     variables = {
       TERMINAL = "alacritty";
@@ -75,32 +73,6 @@
     # Add shells to /etc/shells
     shells = with pkgs; [ fish ];
   };
-
-  hardware = {
-    # Enable PulseAudio
-    pulseaudio = {
-      enable = true;
-      support32Bit = true;
-      package = pkgs.pulseaudioFull; # enable extra codecs
-    };
-    # Enable bluetooth
-    bluetooth.enable = true;
-  };
-
-  # services.printing.enable = true;
-  # sound.enable = true;
-  # security.rtkit.enable = true;
-  # services.pipewire = {
-  #   enable = true;
-  #   alsa.enable = true;
-  #   alsa.support32Bit = true;
-  #   pule.enable = true;
-
-  # for jack apps
-  # jack.enable = true
-
-  # media-session.enable = true;
-  # };
 
   nix = {
     # Setup flakes
@@ -114,6 +86,11 @@
       dates = "weekly";
       options = "--delete-older-than 7d";
     };
+  };
+
+  nixpkgs.config = {
+    # Allow unfree packages
+    allowUnfree = true;
   };
 
   system = {
