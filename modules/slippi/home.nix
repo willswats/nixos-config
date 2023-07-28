2c85f95778db53697a243c315d7899a4a32c7cbc
willswats <williamstuwatson@gmail.com>
2023-07-28 14:15:09 +0100

    Add comments

--- modules/slippi/home.nix
+++ modules/slippi/home.nix
@@ -6,8 +6,8 @@
       user = "project-slippi";
       project = "Ishiiruka";
       assetName = "Slippi_Online-x86_64.AppImage";
-      version = "3.1.0";
-      hash = "039qm941xbl97zvvv0q6480fps4w1a0n71sk1wiacs6n4gm2bs6f";
+      version = "3.1.0"; # Each new version will return a different hash
+      hash = "039qm941xbl97zvvv0q6480fps4w1a0n71sk1wiacs6n4gm2bs6f"; # nix-prefetch-url
     in
     [
       (pkgs.appimageTools.wrapType2
