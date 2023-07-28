20206aa8a678c5c87595fd919d5b49c55ba9289a
willswats <williamstuwatson@gmail.com>
2023-07-28 14:09:43 +0100

    Cleanup slippi module

--- modules/slippi/home.nix
+++ modules/slippi/home.nix
@@ -3,25 +3,23 @@
 {
   home.packages =
     let
-      app_name = "Slippi_Online-x86_64.AppImage";
-      gh_proj = "Ishiiruka";
-      gh_user = "project-slippi";
+      user = "project-slippi";
+      project = "Ishiiruka";
+      assetName = "Slippi_Online-x86_64.AppImage";
       version = "3.1.0";
       hash = "039qm941xbl97zvvv0q6480fps4w1a0n71sk1wiacs6n4gm2bs6f";
     in
     [
       (pkgs.appimageTools.wrapType2
         {
-          name = "slippi-online";
+          name = "slippi";
           extraPkgs = pkgs: [
             pkgs.gmp
             pkgs.mpg123
             pkgs.libmpg123
           ];
           src = builtins.fetchurl {
-            url =
-              "https://github.com/${gh_user}/${gh_proj}/releases/download/"
-              + "v${version}/${app_name}";
+            url = "https://github.com/${user}/${project}/releases/download/v${version}/${assetName}";
             sha256 = "${hash}";
           };
         })
