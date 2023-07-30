6a4b9a736d7f5b38824451a04016ec751b6c8d4d
willswats <williamstuwatson@gmail.com>
2023-07-30 17:48:06 +0100

    Theme flameshot

--- modules/i3/flameshot/home.nix
+++ modules/i3/flameshot/home.nix
@@ -1,10 +1,16 @@
 { ... }:
 
+let
+  blue = "#89b4fa";
+  base = "#1e1e2e";
+in
 {
   services.flameshot = {
     enable = true;
     settings = {
       General = {
+        uiColor = base;
+        contrastUiColor = blue;
         showStartupLaunchMessage = false;
       };
     };
