87264856781f32e23fd47af89f2c35389fbf49eb
willswats <williamstuwatson@gmail.com>
2023-07-30 20:05:56 +0100

    Set force on osu input

--- modules/games/osu/home.nix
+++ modules/games/osu/home.nix
@@ -1,65 +1,68 @@
 { ... }:
 
 {
-  home.file.".local/share/osu/input.json".text = ''
-    {
-      "InputHandlers": [
-        {
-          "$type": "osu.Framework.Input.Handlers.Keyboard.KeyboardHandler, osu.Framework",
-          "Description": "Keyboard",
-          "IsActive": true,
-          "Enabled": true
-        },
-        {
-          "$type": "osu.Framework.Input.Handlers.Tablet.OpenTabletDriverHandler, osu.Framework",
-          "Description": "Tablet",
-          "IsActive": true,
-          "AreaOffset": {
-            "x": 127.0,
-            "y": 79.375
+  home.file.".local/share/osu/input.json" = {
+    force = true;
+    text = ''
+      {
+        "InputHandlers": [
+          {
+            "$type": "osu.Framework.Input.Handlers.Keyboard.KeyboardHandler, osu.Framework",
+            "Description": "Keyboard",
+            "IsActive": true,
+            "Enabled": true
           },
-          "AreaSize": {
-            "x": 159.50275,
-            "y": 89.72029
+          {
+            "$type": "osu.Framework.Input.Handlers.Tablet.OpenTabletDriverHandler, osu.Framework",
+            "Description": "Tablet",
+            "IsActive": true,
+            "AreaOffset": {
+              "x": 127.0,
+              "y": 79.375
+            },
+            "AreaSize": {
+              "x": 159.50275,
+              "y": 89.72029
+            },
+            "Rotation": 0.0,
+            "Tablet": {
+              "Name": "Huion HS610",
+              "Size": {
+                "x": 254.0,
+                "y": 158.75
+              }
+            },
+            "Enabled": true
           },
-          "Rotation": 0.0,
-          "Tablet": {
-            "Name": "Huion HS610",
-            "Size": {
-              "x": 254.0,
-              "y": 158.75
-            }
+          {
+            "$type": "osu.Framework.Input.Handlers.Mouse.MouseHandler, osu.Framework",
+            "UseRelativeMode": false,
+            "Sensitivity": 1.0,
+            "Description": "Mouse",
+            "IsActive": true,
+            "Enabled": true
           },
-          "Enabled": true
-        },
-        {
-          "$type": "osu.Framework.Input.Handlers.Mouse.MouseHandler, osu.Framework",
-          "UseRelativeMode": false,
-          "Sensitivity": 1.0,
-          "Description": "Mouse",
-          "IsActive": true,
-          "Enabled": true
-        },
-        {
-          "$type": "osu.Framework.Input.Handlers.Touch.TouchHandler, osu.Framework",
-          "IsActive": true,
-          "Description": "Touch",
-          "Enabled": true
-        },
-        {
-          "$type": "osu.Framework.Input.Handlers.Joystick.JoystickHandler, osu.Framework",
-          "DeadzoneThreshold": 0.099999994,
-          "Description": "Joystick / Gamepad",
-          "IsActive": true,
-          "Enabled": true
-        },
-        {
-          "$type": "osu.Framework.Input.Handlers.Midi.MidiHandler, osu.Framework",
-          "Description": "MIDI",
-          "IsActive": true,
-          "Enabled": true
-        }
-      ]
-    }
-  '';
+          {
+            "$type": "osu.Framework.Input.Handlers.Touch.TouchHandler, osu.Framework",
+            "IsActive": true,
+            "Description": "Touch",
+            "Enabled": true
+          },
+          {
+            "$type": "osu.Framework.Input.Handlers.Joystick.JoystickHandler, osu.Framework",
+            "DeadzoneThreshold": 0.099999994,
+            "Description": "Joystick / Gamepad",
+            "IsActive": true,
+            "Enabled": true
+          },
+          {
+            "$type": "osu.Framework.Input.Handlers.Midi.MidiHandler, osu.Framework",
+            "Description": "MIDI",
+            "IsActive": true,
+            "Enabled": true
+          }
+        ]
+      }
+    '';
+  };
 }
