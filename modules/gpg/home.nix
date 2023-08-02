8878802c7cc731b44a5e3bb2b46e291ac31a7bd2
willswats <willswats@users.noreply.github.com>
2023-08-02 17:30:29 +0100

    Set defaultCache

--- modules/gpg/home.nix
+++ modules/gpg/home.nix
@@ -9,6 +9,8 @@
     enable = true;
     enableFishIntegration = true;
     enableSshSupport = true;
+    defaultCacheTtl = 86400; # 86400 seconds - 24 hours
+    defaultCacheTtlSsh = 86400; # 86400 seconds - 24 hours
     pinentryFlavor = "gtk2";
   };
 }
