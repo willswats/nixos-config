ffe78424e084c7d97a177ec5535fb39b772780d9
willswats <willswats@users.noreply.github.com>
2023-08-03 17:25:12 +0100

    Update osu module

--- modules/games/osu/home.nix
+++ modules/games/osu/home.nix
@@ -1,72 +1,34 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    osu-lazer-bin
-  ];
-
-  home.file.".local/share/osu/input.json" = {
-    force = true;
-    text = ''
-      {
-        "InputHandlers": [
+  home.packages =
+    [
+      (
+        let
+          user = "ppy";
+          project = "osu";
+          assetName = "osu.AppImage";
+          version = "2023.803.0"; # Pin the version as each new version will return a different hash
+          hash = "01gjrjakd08kg5g08pkn451q6yw8dgsh0kapb41i31q42bp67vvw"; # nix-prefetch-url https://github.com/ppy/osu/releases/download/2023.803.0/osu.AppImage
+        in
+        pkgs.appimageTools.wrapType2
           {
-            "$type": "osu.Framework.Input.Handlers.Keyboard.KeyboardHandler, osu.Framework",
-            "Description": "Keyboard",
-            "IsActive": true,
-            "Enabled": true
-          },
-          {
-            "$type": "osu.Framework.Input.Handlers.Tablet.OpenTabletDriverHandler, osu.Framework",
-            "Description": "Tablet",
-            "IsActive": true,
-            "AreaOffset": {
-              "x": 127.0,
-              "y": 79.375
-            },
-            "AreaSize": {
-              "x": 159.50275,
-              "y": 89.72029
-            },
-            "Rotation": 0.0,
-            "Tablet": {
-              "Name": "Huion HS610",
-              "Size": {
-                "x": 254.0,
-                "y": 158.75
-              }
-            },
-            "Enabled": true
-          },
-          {
-            "$type": "osu.Framework.Input.Handlers.Mouse.MouseHandler, osu.Framework",
-            "UseRelativeMode": false,
-            "Sensitivity": 1.0,
-            "Description": "Mouse",
-            "IsActive": true,
-            "Enabled": true
-          },
-          {
-            "$type": "osu.Framework.Input.Handlers.Touch.TouchHandler, osu.Framework",
-            "IsActive": true,
-            "Description": "Touch",
-            "Enabled": true
-          },
-          {
-            "$type": "osu.Framework.Input.Handlers.Joystick.JoystickHandler, osu.Framework",
-            "DeadzoneThreshold": 0.099999994,
-            "Description": "Joystick / Gamepad",
-            "IsActive": true,
-            "Enabled": true
-          },
-          {
-            "$type": "osu.Framework.Input.Handlers.Midi.MidiHandler, osu.Framework",
-            "Description": "MIDI",
-            "IsActive": true,
-            "Enabled": true
+            name = "osu!";
+            extraPkgs = pkgs: with pkgs; [ icu ];
+            src = builtins.fetchurl {
+              url = "https://github.com/${user}/${project}/releases/download/${version}/${assetName}";
+              sha256 = "${hash}";
+            };
           }
-        ]
-      }
-    '';
-  };
+      )
+    ];
+
+  home.file.".local/share/icons/lazer.png".source = ./lazer.png;
+  home.file.".local/share/applications/osu.desktop".text = ''
+    [Desktop Entry]
+    Name=osu!
+    Exec=osu!
+    Type=Application
+    Icon=lazer
+  '';
 }
