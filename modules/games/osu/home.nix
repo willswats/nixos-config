eef332b28ef953ebeab047d267c09f374757b6bf
willswats <willswats@users.noreply.github.com>
2023-08-04 13:23:03 +0100

    Revert "Remove tablet setup"
    This reverts commit 61839f965d70743d9fe8d408ca7c26a184a94afc.

--- modules/games/osu/home.nix
+++ modules/games/osu/home.nix
@@ -32,4 +32,71 @@
     Type=Application
     Icon=lazer
   '';
+
+  # Setup tablet
+  home.file.".local/share/osu/input.json" = {
+    force = true;
+    text = ''
+        {
+          "InputHandlers": [
+            {
+              "$type": "osu.Framework.Input.Handlers.Keyboard.KeyboardHandler, osu.Framework",
+              "Description": "Keyboard",
+              "IsActive": true,
+              "Enabled": true
+            },
+            {
+              "$type": "osu.Framework.Input.Handlers.Tablet.OpenTabletDriverHandler, osu.Framework",
+              "Description": "Tablet",
+              "IsActive": true,
+              "AreaOffset": {
+                "x": 127.0,
+                "y": 79.375
+              },
+              "AreaSize": {
+                "x": 160.26843,
+                "y": 90.15099
+              },
+              "Rotation": 0.0,
+              "Tablet": {
+                "Name": "Huion HS610",
+                "Size": {
+                  "x": 254.0,
+                  "y": 158.75
+                }
+              },
+              "Enabled": true
+            },
+            {
+              "$type": "osu.Framework.Input.Handlers.Mouse.MouseHandler, osu.Framework",
+              "UseRelativeMode": false,
+              "Sensitivity": 1.0,
+              "Description": "Mouse",
+              "IsActive": true,
+              "Enabled": true
+            },
+            {
+              "$type": "osu.Framework.Input.Handlers.Touch.TouchHandler, osu.Framework",
+              "IsActive": true,
+              "Description": "Touch",
+              "Enabled": true
+            },
+            {
+              "$type": "osu.Framework.Input.Handlers.Joystick.JoystickHandler, osu.Framework",
+              "DeadzoneThreshold": 0.099999994,
+              "Description": "Joystick / Gamepad",
+              "IsActive": true,
+              "Enabled": true
+            },
+            {
+              "$type": "osu.Framework.Input.Handlers.Midi.MidiHandler, osu.Framework",
+              "Description": "MIDI",
+              "IsActive": true,
+              "Enabled": true
+            }
+          ]
+        }
+      }
+    '';
+  };
 }
