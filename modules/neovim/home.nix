c0fbe5fc77db19f651a756db7120841f5f99e0ed
willswats <willswats@users.noreply.github.com>
2023-08-04 14:53:48 +0100

    Format neovim module

--- modules/neovim/home.nix
+++ modules/neovim/home.nix
@@ -1,21 +1,21 @@
 { pkgs, ... }:
 
 {
-home.packages = with pkgs; [
-      fd # Telescope dependency
-      ripgrep # Telescope dependency
-      gcc # rustup dependency
-      rustup # Provides rust LSP
-      nil # Nix LSP
-      nixpkgs-fmt # Nix formatter
-      lua-language-server # Lua language server and formatter
-      nodePackages.typescript-language-server # TypeScript LSP
-      nodePackages.prettier # Code formatter for many languages
-      nodePackages.markdownlint-cli # Markdown linter
-      nodePackages.eslint # JS, TS Linter
-      marksman # Markdown lsp
-      trash-cli # Required for nvim-tree trash
-];
+  home.packages = with pkgs; [
+    fd # Telescope dependency
+    ripgrep # Telescope dependency
+    gcc # rustup dependency
+    rustup # Provides rust LSP
+    nil # Nix LSP
+    nixpkgs-fmt # Nix formatter
+    lua-language-server # Lua language server and formatter
+    nodePackages.typescript-language-server # TypeScript LSP
+    nodePackages.prettier # Code formatter for many languages
+    nodePackages.markdownlint-cli # Markdown linter
+    nodePackages.eslint # JS, TS Linter
+    marksman # Markdown lsp
+    trash-cli # Required for nvim-tree trash
+  ];
 
   programs.neovim = {
     enable = true;
