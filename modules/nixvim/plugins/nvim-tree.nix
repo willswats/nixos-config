4a2d0c1eef729ad7ebf350c26b624ada25368981
willswats <willswats@users.noreply.github.com>
2023-08-05 03:54:32 +0100

    Add nvim tree keymaps

--- modules/nixvim/plugins/nvim-tree.nix
+++ modules/nixvim/plugins/nvim-tree.nix
@@ -12,6 +12,7 @@
     hijackCursor = true;
     hijackNetrw = true;
     syncRootWithCwd = true;
+    trash.cmd = "trash";
     view = {
       float = {
         enable = true;
@@ -23,6 +24,23 @@
         };
       };
     };
-    trash.cmd = "trash";
+    onAttach = {
+      __raw = ''
+        function(bufnr)
+          local api = require('nvim-tree.api')
+
+          local function opts(desc)
+            return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+          end
+
+          api.config.mappings.default_on_attach(bufnr)
+
+          vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
+          vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
+          vim.keymap.set('n', '<CR>', api.tree.change_root_to_node, opts('CD'))
+          vim.keymap.set('n', 'd', api.fs.trash, opts('Trash'))
+        end
+      '';
+    };
   };
 }
