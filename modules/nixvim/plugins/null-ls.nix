2ff062c73ed8192a83639f8ccddedf0aec307efb
willswats <willswats@users.noreply.github.com>
2023-08-07 12:17:24 +0100

    Add null-ls config

--- modules/nixvim/plugins/null-ls.nix
+++ modules/nixvim/plugins/null-ls.nix
@@ -1,19 +1,32 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    nixpkgs-fmt # Nix formatter
-    nodePackages.prettier # Code formatter for many languages
-    nodePackages.markdownlint-cli # Markdown linter
-  ];
+  programs.nixvim = {
+    extraPackages = with pkgs; [
+      nixpkgs-fmt # Nix formatter
+      nodePackages.prettier # Code formatter for many languages
+      nodePackages.markdownlint-cli # Markdown linter
+    ];
+    # Not defined using the module as `diagnostics.markdownlint` does not exist in it
+    # https://github.com/nix-community/nixvim/issues/97
+    extraPlugins = with pkgs.vimPlugins; [ null-ls-nvim ];
+    extraConfigLua = ''
+      local status_ok_null_ls, null_ls = pcall(require, "null-ls")
+      if not status_ok_null_ls then
+        return
+      end
 
-  programs.nixvim.plugins.null-ls = {
-    enable = true;
-    sources = {
-      formatting = {
-        prettier.enable = true;
-        nixfmt.enable = true;
-      };
-    };
+      local formatting = null_ls.builtins.formatting
+      local diagnostics = null_ls.builtins.diagnostics
+
+      null_ls.setup({
+        debug = false,
+        sources = {
+          formatting.prettier,
+          formatting.nixpkgs_fmt,
+          diagnostics.markdownlint.with({ extra_args = { "--disable", "MD013" } })
+        },
+      })
+    '';
   };
 }
