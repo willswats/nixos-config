770f1337d27ff7d70971ed9043d5ce2cb77cf918
willswats <willswats@users.noreply.github.com>
2023-08-07 12:23:24 +0100

    Move packages back to home

--- modules/nixvim/plugins/null-ls.nix
+++ modules/nixvim/plugins/null-ls.nix
@@ -1,12 +1,13 @@
 { pkgs, ... }:
 
 {
+  home.packages = with pkgs; [
+    nixpkgs-fmt # Nix formatter
+    nodePackages.prettier # Code formatter for many languages
+    nodePackages.markdownlint-cli # Markdown linter
+  ];
+
   programs.nixvim = {
-    extraPackages = with pkgs; [
-      nixpkgs-fmt # Nix formatter
-      nodePackages.prettier # Code formatter for many languages
-      nodePackages.markdownlint-cli # Markdown linter
-    ];
     # Not defined using the module as `diagnostics.markdownlint` does not exist in it
     # https://github.com/nix-community/nixvim/issues/97
     extraPlugins = with pkgs.vimPlugins; [ null-ls-nvim ];
