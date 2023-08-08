c922d4697720d5b5c77fab49fb87a4f055e5682c
willswats <willswats@users.noreply.github.com>
2023-08-08 16:03:55 +0100

    Add lualine config

--- modules/development/neovim/plugins/lualine.nix
+++ modules/development/neovim/plugins/lualine.nix
@@ -1,19 +1,56 @@
-{ ... }:
+{ pkgs, ... }:
 
-let icons = import ../icons.nix;
-in {
-  programs.nixvim.plugins.lualine = {
-    enable = true;
-    componentSeparators = {
-      left = icons.ui.LineMiddle;
-      right = icons.ui.LineMiddle;
-    };
-    sectionSeparators = {
-      left = "";
-      right = "";
-    };
-    # +-------------------------------------------------+
-    # | A | B | C                             X | Y | Z |
-    # +-------------------------------------------------+
+{
+  programs.nixvim = {
+    extraPlugins = with pkgs.vimPlugins; [ lualine-nvim ];
+    extraConfigLua = ''
+      local lualine = require("lualine")
+
+      local branch = {
+        "branch",
+        icon = ""
+      }
+
+      local diagnostics = {
+        "diagnostics",
+        symbols = {
+          error = "" .. " ",
+          warn = "" .. " ",
+          hint = "󰌶" .. " "
+        },
+      }
+
+      local diff = {
+        "diff",
+        symbols = {
+          added = "" .. " ",
+          modified = "" .. " ",
+          removed = "" .. " "
+        },
+      }
+
+      lualine.setup({
+        options = {
+          component_separators = { left = "│", right = "│" },
+          section_separators = { left = "", right = "" },
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { branch, diff, diagnostics },
+          lualine_c = { "filename" },
+          lualine_x = { "encoding", "filetype" },
+          lualine_y = { "progress" },
+          lualine_z = { "location" }
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { "filename" },
+          lualine_x = { "location" },
+          lualine_y = {},
+          lualine_z = {},
+        },
+      })
+    '';
   };
 }
