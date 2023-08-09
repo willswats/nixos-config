de3fa331b041aa37b06555a9024108b8d8d44eb0
willswats <willswats@users.noreply.github.com>
2023-08-09 15:13:15 +0100

    Remove cmp has_words_before

--- modules/development/neovim/plugins/nvim-cmp.nix
+++ modules/development/neovim/plugins/nvim-cmp.nix
@@ -3,7 +3,6 @@
 let icons = import ../icons.nix;
 in {
   programs.nixvim = {
-    extraPlugins = with pkgs.vimPlugins; [ friendly-snippets ];
     plugins = {
       cmp-nvim-lsp.enable = true;
       cmp-buffer.enable = true;
@@ -28,8 +27,6 @@ in {
                   cmp.select_next_item()
                 elseif luasnip.expand_or_jumpable() then
                   luasnip.expand_or_jump()
-                elseif has_words_before() then
-                  cmp.complete()
                 else
                   fallback()
                 end
@@ -73,12 +70,8 @@ in {
         ];
       };
     };
+    extraPlugins = with pkgs.vimPlugins; [ friendly-snippets ];
     extraConfigLua = ''
-      local function has_words_before()
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
-      end
-
       require("luasnip.loaders.from_vscode").lazy_load()
     '';
   };
