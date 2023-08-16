701b6d9ebf406d2676747d715c19f8fae41b89d9
willswats <willswats@users.noreply.github.com>
2023-08-16 22:54:51 +0100

    Add gtk cursor and font

--- modules/theme/cursor/home.nix
+++ modules/theme/cursor/home.nix
@@ -3,8 +3,8 @@
 {
   home = {
     pointerCursor = {
-      x11 = { enable = true; };
-      gtk = { enable = true; };
+      x11.enable = true;
+      gtk.enable = true;
       name = "Catppuccin-Mocha-Dark-Cursors";
       package = pkgs.catppuccin-cursors.mochaDark;
       size = 32; # 24 32 48 64
--- modules/theme/gtk/home.nix
+++ modules/theme/gtk/home.nix
@@ -1,40 +1,9 @@
 { pkgs, host, ... }:
 
 {
-  gtk = {
-    enable = true;
-    theme = {
-      name = "Catppuccin-Mocha-Standard-Blue-dark";
-      package = pkgs.catppuccin-gtk.override {
-        accents = [ "blue" ];
-        size = "standard";
-        variant = "mocha";
-      };
-    };
-    iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.catppuccin-papirus-folders.override {
-        accent = "blue";
-        flavor = "mocha";
-      };
-    };
-    gtk2 = { extraConfig = "gtk-error-bell = 0"; };
-    gtk3 = {
-      bookmarks = host.bookmarks;
-      extraConfig = {
-        gtk-error-bell = 0;
-      };
-      # Remove black borders https://github.com/catppuccin/gtk/issues/6#issuecomment-1443573299
-      extraCss = ''
-        menu,
-        .csd .menu,
-        .csd .dropdown,
-        .csd .context-menu {
-          border-radius: 0px;
-        }
-      '';
-    };
-    gtk4 = {
+  gtk =
+    let
+      # Disable error sound
       extraConfig = { gtk-error-bell = 0; };
       # Remove black borders https://github.com/catppuccin/gtk/issues/6#issuecomment-1443573299
       extraCss = ''
@@ -45,8 +14,45 @@
           border-radius: 0px;
         }
       '';
+    in
+    {
+      enable = true;
+      font = {
+        name = "Hack";
+        package = pkgs.nerdfonts.override { fonts = [ "Hack" ]; };
+        size = 10;
+      };
+      theme = {
+        name = "Catppuccin-Mocha-Standard-Blue-dark";
+        package = pkgs.catppuccin-gtk.override {
+          accents = [ "blue" ];
+          size = "standard";
+          variant = "mocha";
+        };
+      };
+      iconTheme = {
+        name = "Papirus-Dark";
+        package = pkgs.catppuccin-papirus-folders.override {
+          accent = "blue";
+          flavor = "mocha";
+        };
+      };
+      cursorTheme = {
+        name = "Catppuccin-Mocha-Dark-Cursors";
+        package = pkgs.catppuccin-cursors.mochaDark;
+        size = 32; # 24 32 48 64
+      };
+      gtk2 = { extraConfig = "gtk-error-bell = 0"; };
+      gtk3 = {
+        bookmarks = host.bookmarks;
+        extraConfig = extraConfig;
+        extraCss = extraCss;
+      };
+      gtk4 = {
+        extraConfig = extraConfig;
+        extraCss = extraCss;
+      };
     };
-  };
 
   # GTK4 Theme
   home.sessionVariables.GTK_THEME = "Catppuccin-Mocha-Standard-Blue-dark";
