3fcf3e71d20835170809b10f595402b60223d2ca
willswats <willswats@users.noreply.github.com>
2023-08-20 20:23:38 +0100

    Change area size

--- modules/games/osu/home.nix
+++ modules/games/osu/home.nix
@@ -37,65 +37,64 @@
   home.file.".local/share/osu/input.json" = {
     force = true;
     text = ''
-        {
-          "InputHandlers": [
-            {
-              "$type": "osu.Framework.Input.Handlers.Keyboard.KeyboardHandler, osu.Framework",
-              "Description": "Keyboard",
-              "IsActive": true,
-              "Enabled": true
+      {
+        "InputHandlers": [
+          {
+            "$type": "osu.Framework.Input.Handlers.Keyboard.KeyboardHandler, osu.Framework",
+            "Description": "Keyboard",
+            "IsActive": true,
+            "Enabled": true
+          },
+          {
+            "$type": "osu.Framework.Input.Handlers.Tablet.OpenTabletDriverHandler, osu.Framework",
+            "Description": "Tablet",
+            "IsActive": true,
+            "AreaOffset": {
+              "x": 127.0,
+              "y": 79.375
             },
-            {
-              "$type": "osu.Framework.Input.Handlers.Tablet.OpenTabletDriverHandler, osu.Framework",
-              "Description": "Tablet",
-              "IsActive": true,
-              "AreaOffset": {
-                "x": 127.0,
-                "y": 79.375
-              },
-              "AreaSize": {
-                "x": 160.26843,
-                "y": 90.15099
-              },
-              "Rotation": 0.0,
-              "Tablet": {
-                "Name": "Huion HS610",
-                "Size": {
-                  "x": 254.0,
-                  "y": 158.75
-                }
-              },
-              "Enabled": true
+            "AreaSize": {
+              "x": 180.42426,
+              "y": 101.48864
             },
-            {
-              "$type": "osu.Framework.Input.Handlers.Mouse.MouseHandler, osu.Framework",
-              "UseRelativeMode": false,
-              "Sensitivity": 1.0,
-              "Description": "Mouse",
-              "IsActive": true,
-              "Enabled": true
+            "Rotation": 0.0,
+            "Tablet": {
+              "Name": "Huion HS610",
+              "Size": {
+                "x": 254.0,
+                "y": 158.75
+              }
             },
-            {
-              "$type": "osu.Framework.Input.Handlers.Touch.TouchHandler, osu.Framework",
-              "IsActive": true,
-              "Description": "Touch",
-              "Enabled": true
-            },
-            {
-              "$type": "osu.Framework.Input.Handlers.Joystick.JoystickHandler, osu.Framework",
-              "DeadzoneThreshold": 0.099999994,
-              "Description": "Joystick / Gamepad",
-              "IsActive": true,
-              "Enabled": true
-            },
-            {
-              "$type": "osu.Framework.Input.Handlers.Midi.MidiHandler, osu.Framework",
-              "Description": "MIDI",
-              "IsActive": true,
-              "Enabled": true
-            }
-          ]
-        }
+            "Enabled": true
+          },
+          {
+            "$type": "osu.Framework.Input.Handlers.Mouse.MouseHandler, osu.Framework",
+            "UseRelativeMode": false,
+            "Sensitivity": 1.0,
+            "Description": "Mouse",
+            "IsActive": true,
+            "Enabled": true
+          },
+          {
+            "$type": "osu.Framework.Input.Handlers.Touch.TouchHandler, osu.Framework",
+            "IsActive": true,
+            "Description": "Touch",
+            "Enabled": true
+          },
+          {
+            "$type": "osu.Framework.Input.Handlers.Joystick.JoystickHandler, osu.Framework",
+            "DeadzoneThreshold": 0.099999994,
+            "Description": "Joystick / Gamepad",
+            "IsActive": true,
+            "Enabled": true
+          },
+          {
+            "$type": "osu.Framework.Input.Handlers.Midi.MidiHandler, osu.Framework",
+            "Description": "MIDI",
+            "IsActive": true,
+            "Enabled": true
+          }
+        ]
       }
     '';
   };
