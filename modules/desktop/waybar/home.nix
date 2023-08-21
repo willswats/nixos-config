8eab46443abee810d6c014db4c907f012e806ebb
willswats <willswats@users.noreply.github.com>
2023-08-21 14:12:53 +0100

    Remove redundant variables

--- modules/desktop/waybar/home.nix
+++ modules/desktop/waybar/home.nix
@@ -84,12 +84,7 @@
           overlay0 = "#6c7086";
 
           blue = "#89b4fa";
-          lavender = "#b4befe";
-          sapphire = "#74c7ec";
-          sky = "#89dceb";
-          teal = "#94e2d5";
           green = "#a6e3a1";
-          yellow = "#f9e2af";
           peach = "#fab387";
           maroon = "#eba0ac";
           red = "#f38ba8";
