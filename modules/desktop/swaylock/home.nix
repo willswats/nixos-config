2550beb4d1f6584848527aa085ff1dfd6905775e
willswats <willswats@users.noreply.github.com>
2023-08-21 18:14:54 +0100

    Add daemonize to swaylock

--- modules/desktop/swaylock/home.nix
+++ modules/desktop/swaylock/home.nix
@@ -7,6 +7,7 @@
       color = "1e1e2e";
       font-size = 32;
       indicator-radius = 100;
+      daemonize = true; # Fix screen locking multiple times - https://github.com/swaywm/swaylock/issues/86
     };
   };
 }
