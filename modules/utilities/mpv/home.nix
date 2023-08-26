cffd4d2da5ba304f50db7b057325140ca5f750f4
willswats <willswats@users.noreply.github.com>
2023-08-26 23:12:33 +0100

    Add paste to mpv

--- modules/utilities/mpv/home.nix
+++ modules/utilities/mpv/home.nix
@@ -27,4 +27,31 @@
       };
     };
   };
+
+  xdg.configFile."mpv/scripts/paste.lua" = {
+    text = ''
+      local mp = require "mp"
+
+      local opts = {
+        -- Key used for pasting
+        key_paste = "Ctrl+v",
+        -- Duration of osd messages
+        osd_message_duration = 5
+      }
+
+      local function paste()
+        local clipboard = mp.command_native {
+          name = "subprocess",
+          playback_only = false,
+          args = { "wl-paste", "--no-newline" },
+          capture_stdout = true
+        }.stdout
+        mp.commandv("loadfile", clipboard)
+        mp.osd_message("Pasted", opts.osd_message_duration)
+      end
+
+      mp.add_key_binding(opts.key_paste, "paste", paste)
+    '';
+
+  };
 }
