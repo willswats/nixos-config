de34fd44778df41b3dc77045f908ee7793b7b465
willswats <willswats@users.noreply.github.com>
2023-08-26 03:36:31 +0100

    Seperate and fix mpv-youtube-search

--- modules/utilities/mpv/home.nix
+++ modules/utilities/mpv/home.nix
@@ -1,16 +1,18 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    (callPackage ../../../pkgs/mpv-youtube-search { })
-  ];
-
   programs.mpv = {
     enable = true;
     config = { volume = 50; };
     scripts = with pkgs; [
-      # (callPackage ../../../pkgs/mpv-user-input { })
-      (callPackage ../../../pkgs/mpv-youtube-search { })
+      (callPackage ../../../pkgs/mpv-user-input { })
+      (callPackage ../../../pkgs/mpv-youtube-search {
+        mpv-user-input = {
+          src = {
+            outPath = (callPackage ../../../pkgs/mpv-user-input { });
+          };
+        };
+      })
       mpvScripts.mpv-playlistmanager
     ];
   };
