874b8bab124d7863ceed4a4d0fd7e1927fd93aa9
willswats <willswats@users.noreply.github.com>
2023-08-28 15:59:49 +0100

    Revert "Remove extraCss"
    This reverts commit 624a37ab685a2cfc736e4545d45f95a1cc2df565.

--- modules/theme/gtk/home.nix
+++ modules/theme/gtk/home.nix
@@ -5,6 +5,15 @@
     let
       # Disable error sound
       extraConfig = { gtk-error-bell = 0; };
+      # Remove black borders https://github.com/catppuccin/gtk/issues/6#issuecomment-1443573299
+      extraCss = ''
+        menu,
+        .csd .menu,
+        .csd .dropdown,
+        .csd .context-menu {
+          border-radius: 0px;
+        }
+      '';
     in
     {
       enable = true;
@@ -32,9 +41,11 @@
       gtk3 = {
         bookmarks = host.bookmarks;
         extraConfig = extraConfig;
+        extraCss = extraCss;
       };
       gtk4 = {
         extraConfig = extraConfig;
+        extraCss = extraCss;
       };
     };
 
