2454a7ba3546da592bc7442f9d6ef5a55c93a8dc
willswats <willswats@users.noreply.github.com>
2023-08-29 12:04:54 +0100

    Revert "Revert "Add vscode for unity""
    This reverts commit 866f24e5b254a60d03503366b57f1c2ba4015e93.

--- modules/development/unity/home.nix
+++ modules/development/unity/home.nix
@@ -1,8 +1,10 @@
 { pkgs, ... }:
 
 {
+  imports = [ ../vscode/home.nix ];
+
   home.packages = with pkgs; [
     unityhub
-    vscode # Needed to generate the project files needed for OmniSharp to find the root dir in Neovim
+    mono
   ];
 }
