27e7eb5cd0364c1f0d696f43f23ac7b61644976c
willswats <willswats@users.noreply.github.com>
2023-09-14 09:45:22 +0100

    Update autoexec

--- modules/games/csgo/home.nix
+++ modules/games/csgo/home.nix
@@ -1,4 +1,4 @@
-{ ... }: 
+{ ... }:
 
 {
   home.file.".steam/steam/steamapps/common/Counter-Strike Global Offensive/csgo/cfg/autoexec.cfg".text =
@@ -9,37 +9,42 @@
       # Bindings
       bind mwheeldown +jump
       bind mwheelup +jump
-      bind / "say !r"
+      bind mouse5 slot12
+      bind mouse4 "say !r"
+        
       bindtoggle z voice_enable
       alias "+jumpthrow" "+jump;-attack"; alias "-jumpthrow" "-jump"; bind x "+jumpthrow"
       bind c player_ping
       bind v +voicerecord
-      bind h slot12
+
+      bind alt noclip
+      bind [ "sv_cheats 1; bot_kick; mp_warmup_end; mp_freezetime 0; mp_roundtime_defuse 60; sv_grenade_trajectory 1; sv_grenade_trajectory_time 10; sv_showimpacts 1; ammo_grenade_limit_total 5; sv_infinite_ammo 1;"
+      bind ] "mp_buy_anywhere 1; mp_maxmoney 50000; mp_startmoney 50000; mp_buytime 9999; mp_restartgame 1;"
 
       # Crosshair
       cl_crosshair_drawoutline "1"
-      cl_crosshair_dynamic_maxdist_splitratio "0.0"
+      cl_crosshair_dynamic_maxdist_splitratio "0.35"
       cl_crosshair_dynamic_splitalpha_innermod "1"
-      cl_crosshair_dynamic_splitalpha_outermod "0.300000"
-      cl_crosshair_dynamic_splitdist "5"
+      cl_crosshair_dynamic_splitalpha_outermod "0.5"
+      cl_crosshair_dynamic_splitdist "7"
       cl_crosshair_friendly_warning "1"
       cl_crosshair_outlinethickness "1"
       cl_crosshair_sniper_show_normal_inaccuracy "0"
       cl_crosshair_sniper_width "1"
       cl_crosshair_t "0"
-      cl_crosshairalpha "255.000000"
-      cl_crosshaircolor "5"
-      cl_crosshaircolor_b "0"
-      cl_crosshaircolor_g "255"
-      cl_crosshaircolor_r "0"
+      cl_crosshairalpha "255"
+      cl_crosshaircolor "1"
+      cl_crosshaircolor_b "50"
+      cl_crosshaircolor_g "250"
+      cl_crosshaircolor_r "50"
       cl_crosshairdot "0"
-      cl_crosshairgap "-2"
+      cl_crosshairgap "-1"
       cl_crosshairgap_useweaponvalue "0"
-      cl_crosshairsize "1.5"
+      cl_crosshairsize "2"
       cl_crosshairstyle "4"
-      cl_crosshairthickness "0.5"
+      cl_crosshairthickness "1"
       cl_crosshairusealpha "1"
-      cl_fixedcrosshairgap "-10"
+      cl_fixedcrosshairgap "-1"
 
       # Viewmodel
       cl_viewmodel_shift_left_amt "1.5"
