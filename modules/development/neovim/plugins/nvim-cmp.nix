f639a3571ec3831b5a5f0cb7055f422f06646fec
willswats <willswats@users.noreply.github.com>
2023-11-14 18:33:32 +0000

    Add cmp autopairs config

--- modules/development/neovim/plugins/nvim-cmp.nix
+++ modules/development/neovim/plugins/nvim-cmp.nix
@@ -76,5 +76,14 @@ in {
         ];
       };
     };
+    # Insert `(` after selecting a function or method item
+    extraConfigLua = ''
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      local cmp = require('cmp')
+      cmp.event:on(
+        'confirm_done',
+        cmp_autopairs.on_confirm_done()
+      )
+    '';
   };
 }
