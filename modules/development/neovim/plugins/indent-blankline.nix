e49417c6c949179569f6e696628b8a9c74cc59a3
willswats <willswats@users.noreply.github.com>
2023-11-16 18:26:08 +0000

    Update flake, disable scope indent blankline

--- modules/development/neovim/plugins/indent-blankline.nix
+++ modules/development/neovim/plugins/indent-blankline.nix
@@ -1,10 +1,13 @@
 { ... }:
 
-let icons = import ../icons.nix;
-in {
+let
+  icons = import ../icons.nix;
+in
+{
   programs.nixvim.plugins.indent-blankline = {
     enable = true;
-    char = icons.ui.LineLeft;
-    contextChar = icons.ui.LineLeft;
+    scope.enabled = false;
+    indent.char = icons.ui.LineLeft;
+    scope.char = icons.ui.LineLeft;
   };
 }
