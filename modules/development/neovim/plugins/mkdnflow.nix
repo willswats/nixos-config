9f43df453743d13f9ab2f426474f30fdd100268b
willswats <willswats@users.noreply.github.com>
2023-11-22 22:30:30 +0000

    Update mkdnflow and markdown-preview keymaps

--- modules/development/neovim/plugins/mkdnflow.nix
+++ modules/development/neovim/plugins/mkdnflow.nix
@@ -1,91 +1,51 @@
-{ config, ... }:
+{ ... }:
 
 {
-  programs.nixvim = {
-    keymaps =
-      let
-        cr = "<CR>";
-        cmd = "<CMD>";
+  programs.nixvim.plugins.mkdnflow = {
+    enable = true;
+    toDo = {
+      symbols = [ " " "x" " " ];
+      updateParents = false;
+      notStarted = " ";
+      inProgress = " ";
+      complete = "x";
+    };
 
-        normal =
-          let
-            mode = "n";
-          in
-          [
-            {
-              inherit mode;
-              key = "<leader>c";
-              action = "${cmd}MkdnToggleToDo${cr}";
-              options.desc = "Checkbox toggle";
-            }
-          ];
-
-        visual =
-
-          let
-            mode = "v";
-          in
-          [
-            {
-              inherit mode;
-              key = "<leader>c";
-              action = "${cmd}MkdnToggleToDo${cr}";
-              options.desc = "Checkbox toggle";
-            }
-          ];
-      in
-      config.nixvim.helpers.keymaps.mkKeymaps
-        {
-          options.silent = true;
-        }
-        (normal ++ visual);
-
-    plugins.mkdnflow = {
-      enable = true;
-      toDo = {
-        symbols = [ " " "x" " " ];
-        updateParents = false;
-        notStarted = " ";
-        inProgress = " ";
-        complete = "x";
-      };
-
-      mappings = {
-        MkdnEnter = { modes = [ "n" "v" ]; key = "<CR>"; };
-        MkdnTab = false;
-        MkdnSTab = false;
-        MkdnNextLink = { modes = "n"; key = "<Tab>"; };
-        MkdnPrevLink = { modes = "n"; key = "<S-Tab>"; };
-        MkdnNextHeading = { modes = "n"; key = "]]"; };
-        MkdnPrevHeading = { modes = "n"; key = "[["; };
-        MkdnGoBack = false;
-        MkdnGoForward = false;
-        MkdnFollowLink = false; # see MkdnEnter 
-        MkdnCreateLink = false; # see MkdnEnter 
-        MkdnCreateLinkFromClipboard = false;
-        MkdnDestroyLink = false;
-        MkdnMoveSource = false;
-        MkdnYankAnchorLink = false;
-        MkdnYankFileAnchorLink = false;
-        MkdnIncreaseHeading = { modes = "n"; key = "+"; };
-        MkdnDecreaseHeading = { modes = "n"; key = "-"; };
-        MkdnToggleToDo = false;
-        MkdnNewListItem = false;
-        MkdnNewListItemBelowInsert = { modes = "n"; key = "o"; };
-        MkdnNewListItemAboveInsert = { modes = "n"; key = "O"; };
-        MkdnExtendList = false;
-        MkdnUpdateNumbering = false;
-        MkdnTableNextCell = false;
-        MkdnTablePrevCell = false;
-        MkdnTableNextRow = false;
-        MkdnTablePrevRow = false;
-        MkdnTableNewRowBelow = false;
-        MkdnTableNewRowAbove = false;
-        MkdnTableNewColAfter = false;
-        MkdnTableNewColBefore = false;
-        MkdnFoldSection = false;
-        MkdnUnfoldSection = false;
-      };
+    mappings = {
+      MkdnEnter = { modes = [ "n" "v" ]; key = "<CR>"; };
+      MkdnTab = false;
+      MkdnSTab = false;
+      MkdnNextLink = { modes = "n"; key = "<Tab>"; };
+      MkdnPrevLink = { modes = "n"; key = "<S-Tab>"; };
+      MkdnNextHeading = { modes = "n"; key = "]]"; };
+      MkdnPrevHeading = { modes = "n"; key = "[["; };
+      MkdnGoBack = false;
+      MkdnGoForward = false;
+      MkdnFollowLink = false; # see MkdnEnter 
+      MkdnCreateLink = false; # see MkdnEnter 
+      MkdnCreateLinkFromClipboard = false;
+      MkdnDestroyLink = false;
+      MkdnMoveSource = false;
+      MkdnYankAnchorLink = false;
+      MkdnYankFileAnchorLink = false;
+      MkdnIncreaseHeading = { modes = "n"; key = "+"; };
+      MkdnDecreaseHeading = { modes = "n"; key = "-"; };
+      MkdnToggleToDo = { modes = [ "n" "v" ]; key = "<C-Space>"; };
+      MkdnNewListItem = false;
+      MkdnNewListItemBelowInsert = { modes = "n"; key = "o"; };
+      MkdnNewListItemAboveInsert = { modes = "n"; key = "O"; };
+      MkdnExtendList = false;
+      MkdnUpdateNumbering = false;
+      MkdnTableNextCell = false;
+      MkdnTablePrevCell = false;
+      MkdnTableNextRow = false;
+      MkdnTablePrevRow = false;
+      MkdnTableNewRowBelow = false;
+      MkdnTableNewRowAbove = false;
+      MkdnTableNewColAfter = false;
+      MkdnTableNewColBefore = false;
+      MkdnFoldSection = false;
+      MkdnUnfoldSection = false;
     };
   };
 }
