a9cce7425e8fdb6f07c54e822c80c3d0f18564b5
willswats <willswats@users.noreply.github.com>
2023-12-19 16:13:22 +0000

    Update osu

--- pkgs/osu-lazer-bin/default.nix
+++ pkgs/osu-lazer-bin/default.nix
@@ -8,21 +8,21 @@
 
 let
   pname = "osu-lazer-bin";
-  version = "2023.1218.0";
+  version = "2023.1218.1";
   name = "${pname}-${version}";
 
   osu-lazer-bin-src = {
     aarch64-darwin = {
       url = "https://github.com/ppy/osu/releases/download/${version}/osu.app.Apple.Silicon.zip";
-      sha256 = "sha256-NX5mxFhoKtKjVxoXANjsZUq/MbRpPsOPpo1e2kOQpxc=";
+      sha256 = "sha256-xxWNLP5m/nxGutt/ogcDYcVaiFaesEvZpDdU/ndVRMs=";
     };
     x86_64-darwin = {
       url = "https://github.com/ppy/osu/releases/download/${version}/osu.app.Intel.zip";
-      sha256 = "sha256-NX5mxFhoKtKjVxoXANjsZUq/MbRpPsOPpo1e2kOQpxc=";
+      sha256 = "sha256-xxWNLP5m/nxGutt/ogcDYcVaiFaesEvZpDdU/ndVRMs=";
     };
     x86_64-linux = {
       url = "https://github.com/ppy/osu/releases/download/${version}/osu.AppImage";
-      sha256 = "sha256-NX5mxFhoKtKjVxoXANjsZUq/MbRpPsOPpo1e2kOQpxc=";
+      sha256 = "sha256-xxWNLP5m/nxGutt/ogcDYcVaiFaesEvZpDdU/ndVRMs=";
     };
   }.${stdenv.system} or (throw "${pname}-${version}: ${stdenv.system} is unsupported.");
 
