f19aad732c1d0db79f9bd2e75d0cf5c2d8fb8fa5
willswats <willswats@users.noreply.github.com>
2023-12-22 23:26:33 +0000

    Fix mullvad no internet

--- modules/utilities/mullvad-vpn/default.nix
+++ modules/utilities/mullvad-vpn/default.nix
@@ -3,4 +3,7 @@
 {
   networking.wireguard.enable = true;
   services.mullvad-vpn.enable = true;
+  # Fixes: https://discourse.nixos.org/t/connected-to-mullvadvpn-but-no-internet-connection/35803/12
+  # You can potentially remove this in the latest version: https://github.com/mullvad/mullvadvpn-app/issues/5499
+  networking.resolvconf.enable = false;
 }
