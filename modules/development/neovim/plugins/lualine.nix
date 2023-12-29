5a261d59d99035ee55cdd67664aebb94d8bb93ec
willswats <willswats@users.noreply.github.com>
2023-12-29 17:37:48 +0000

    Add showmode to lualine

--- modules/development/neovim/plugins/lualine.nix
+++ modules/development/neovim/plugins/lualine.nix
@@ -2,51 +2,68 @@
 
 let icons = import ../icons.nix;
 in {
-  programs.nixvim.plugins.lualine = {
-    enable = true;
-    componentSeparators = {
-      left = icons.ui.LineMiddle;
-      right = icons.ui.LineMiddle;
-    };
-    sectionSeparators = {
-      left = "";
-      right = "";
-    };
-    # +-------------------------------------------------+
-    # | A | B | C                             X | Y | Z |
-    # +-------------------------------------------------+
-    sections = {
-      lualine_a = [ "mode" ];
-      lualine_b = [
-        {
-          name = "branch";
-          icon = icons.git.Branch;
-        }
-        {
-          name = "diagnostics";
-          extraConfig = {
-            symbols = {
-              error = "${icons.diagnostics.Error} ";
-              warn = "${icons.diagnostics.Warning} ";
-              hint = "${icons.diagnostics.Hint} ";
+  programs.nixvim = {
+    plugins.lualine = {
+      enable = true;
+      componentSeparators = {
+        left = icons.ui.LineMiddle;
+        right = icons.ui.LineMiddle;
+      };
+      sectionSeparators = {
+        left = "";
+        right = "";
+      };
+      # +-------------------------------------------------+
+      # | A | B | C                             X | Y | Z |
+      # +-------------------------------------------------+
+      sections = {
+        lualine_a = [ "mode" ];
+        lualine_b = [
+          {
+            name = "branch";
+            icon = icons.git.Branch;
+          }
+          {
+            name = "diagnostics";
+            extraConfig = {
+              symbols = {
+                error = "${icons.diagnostics.Error} ";
+                warn = "${icons.diagnostics.Warning} ";
+                hint = "${icons.diagnostics.Hint} ";
+              };
             };
-          };
-        }
-        {
-          name = "diff";
-          extraConfig = {
-            symbols = {
-              added = "${icons.git.LineAdded} ";
-              modified = "${icons.git.LineModified} ";
-              removed = "${icons.git.LineRemoved} ";
+          }
+          {
+            name = "diff";
+            extraConfig = {
+              symbols = {
+                added = "${icons.git.LineAdded} ";
+                modified = "${icons.git.LineModified} ";
+                removed = "${icons.git.LineRemoved} ";
+              };
             };
-          };
-        }
-      ];
-      lualine_c = [ "filename" ];
-      lualine_x = [ "encoding" "filetype" ];
-      lualine_y = [ "progress" ];
-      lualine_z = [ "location" ];
+          }
+        ];
+        lualine_c = [ "filename" ];
+        lualine_y = [ "progress" ];
+        lualine_z = [ "location" ];
+      };
     };
+    # Enable showmode https://github.com/folke/noice.nvim/wiki/A-Guide-to-Messages#showmode
+    # Showmode is needed, because when you record macros it stops cmp https://github.com/hrsh7th/nvim-cmp/issues/1692
+    extraConfigLua = ''
+      require("lualine").setup({
+        sections = {
+          lualine_x = {
+            {
+              require("noice").api.statusline.mode.get,
+              cond = require("noice").api.statusline.mode.has,
+            },
+            "encoding", 
+            "filetype"
+          },
+        },
+      })
+    '';
   };
 }
