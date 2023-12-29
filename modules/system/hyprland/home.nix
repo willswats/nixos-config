4a668a790ba88221c3c101f58f02e70ec1128885
willswats <willswats@users.noreply.github.com>
2023-12-29 18:30:40 +0000

    Add temp exit script

--- modules/system/hyprland/home.nix
+++ modules/system/hyprland/home.nix
@@ -76,6 +76,9 @@ in
       playerctld = "${pkgs.playerctl}/bin/playerctld";
       brightnessctl = "${pkgs.brightnessctl}/bin/brightnessctl";
 
+      # Temporary exit script
+      exit = "kill -9 $(pidof Hyprland)";
+
       colours_start = "0xff";
       mauve = "${colours_start}${globals.colours.mauve}";
       blue = "${colours_start}${globals.colours.blue}";
@@ -208,7 +211,8 @@ in
           "$mod, q, killactive"
           "$mod, f, fullscreen"
 
-          "$mod shift, e, exit"
+          # Temporary exit script instead of "$mod shift, e, exit" until this issue is fixed: https://github.com/hyprwm/Hyprland/issues/3558
+          "$mod shift, e, exec, ${exit}"
           "$mod shift, r, exec, ${hyprctl} reload"
 
           "$mod shift, f, togglefloating"
