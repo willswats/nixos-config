05ad559b2338a20d9afde4e7f105421c952172ea
willswats <willswats@users.noreply.github.com>
2024-01-03 00:52:52 +0000

    Remove lutris, add bottles

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -6,7 +6,6 @@
     ../../modules/system/opentabletdriver/home.nix
     ../../modules/games/steam/home.nix
     ../../modules/games/heroic/home.nix
-    ../../modules/games/lutris/home.nix
     ../../modules/games/r2modman/home.nix
     ../../modules/games/osu/home.nix
     ../../modules/games/ishiiruka/home.nix
@@ -18,6 +17,7 @@
   home.packages = with pkgs; [
     # Utilities
     mangohud
+    bottles
     soundux
     # Games
     airshipper
