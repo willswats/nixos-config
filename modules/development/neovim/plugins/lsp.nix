23dfceaa019632d97651b4791742ce36d7ea71af
willswats <willswats@users.noreply.github.com>
2024-01-05 15:33:04 +0000

    Disable sqls format

--- modules/development/neovim/plugins/lsp.nix
+++ modules/development/neovim/plugins/lsp.nix
@@ -84,6 +84,10 @@ in
 
       lspconfig.sqls.setup {
         capabilities = ${capabilities},
+        on_attach = function(client)
+          client.server_capabilities.documentFormattingProvider = false
+          client.server_capabilities.documentRangeFormattingProvider = false
+        end,
         settings = {
           sqls = {
             connections = {},
