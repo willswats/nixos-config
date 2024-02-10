d32fa694bcc0232b56d980d8335cb171511e5d5c
willswats <willswats@users.noreply.github.com>
2024-02-10 17:50:20 +0000

    Format

--- modules/utilities/mullvad-vpn/default.nix
+++ modules/utilities/mullvad-vpn/default.nix
@@ -4,6 +4,6 @@
   networking.wireguard.enable = true;
   services.mullvad-vpn.enable = true;
   # This fixes no internet connection when connected to Mullvad VPN
-  # # https://discourse.nixos.org/t/connected-to-mullvadvpn-but-no-internet-connection/35803/6
+  # https://discourse.nixos.org/t/connected-to-mullvadvpn-but-no-internet-connection/35803/6
   services.resolved.enable = true;
 }
