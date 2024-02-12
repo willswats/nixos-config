02db4c14ba43398f71aee4900a7d3f1d260f7ad8
willswats <willswats@users.noreply.github.com>
2024-02-12 10:48:58 +0000

    Add yazi keymaps

--- modules/utilities/yazi/home.nix
+++ modules/utilities/yazi/home.nix
@@ -1,27 +1,53 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    ueberzugpp
-  ];
+  home.packages = with pkgs;
+    [
+      ueberzugpp
+    ];
 
   programs.yazi = {
     enable = true;
     enableFishIntegration = true;
+    keymap = {
+      manager.prepend_keymap = [
+        {
+          on = [ "T" ];
+          exec = "tab_close";
+        }
+        {
+          on = [ "<C-s>" ];
+          exec = "shell \"$SHELL\" --block --confirm";
+          desc = "Open shell here";
+        }
+        {
+          on = [ "y" ];
+          exec = [ "yank" "shell --confirm 'for path in \"$@\"; do echo \"file://$path\"; done | wl-copy -t text/uri-list'" ];
+        }
+        {
+          on = [ "l" ];
+          exec = "plugin --sync smart-enter";
+          desc = "Enter the child directory, or open the file";
+        }
+      ];
+      manager.append_keymap = [
+        {
+          on = [ "g" "u" ];
+          exec = "cd ~/Dropbox/Work/Education/University";
+          desc = "Go to University directory";
+        }
+      ];
+    };
   };
 
-  xdg.configFile."yazi/keymap.toml" = {
+  xdg.configFile."yazi/plugins/smart-enter.yazi/init.lua" = {
     text = ''
-      [[manager.prepend_keymap]]
-      on   = [ "<C-s>" ]
-      exec = 'shell "$SHELL" --block --confirm'
-      desc = "Open shell here"
-
-      [[manager.prepend_keymap]]
-      on   = [ "y" ]
-      exec = [ "yank", ''''
-        shell --confirm 'for path in "$@"; do echo "file://$path"; done | wl-copy -t text/uri-list'
-      '''' ]
+      return {
+      	entry = function()
+      		local h = cx.active.current.hovered
+      		ya.manager_emit(h and h.cha.is_dir and "enter" or "open", {})
+      	end,
+      }
     '';
   };
 }
