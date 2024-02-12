5289e53ab2ed3f20ea0e92e4e7118b669fe73415
willswats <willswats@users.noreply.github.com>
2024-02-12 21:43:43 +0000

    Change wezterm settings

--- modules/utilities/wezterm/home.nix
+++ modules/utilities/wezterm/home.nix
@@ -13,7 +13,8 @@
           font = wezterm.font("${fontName}"),
           font_size = 14.0,
           color_scheme = "Catppuccin Mocha",
-          enable_tab_bar = false,
+          use_fancy_tab_bar = false,
+          hide_tab_bar_if_only_one_tab = true,
           window_close_confirmation = "NeverPrompt",
           window_padding = {
             left = 0,
