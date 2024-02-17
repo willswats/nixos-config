29885c9a456922bf1a2ac2d9eea23798ef24e5a8
willswats <willswats@users.noreply.github.com>
2024-02-17 11:22:11 +0000

    Add misc options to fix dpms on

--- modules/system/hyprland/home.nix
+++ modules/system/hyprland/home.nix
@@ -140,6 +140,9 @@ in
         };
 
         misc = {
+          # These options are here, because sometimes the swayidle resumeCommand doesn't run
+          mouse_move_enables_dpms = true;
+          key_press_enables_dpms = true;
           # Fix Firefox flashing default hyprland wallpaper on resize (still occurs, but it's less noticeable)
           # https://github.com/hyprwm/Hyprland/issues/2817
           disable_hyprland_logo = true;
