59e030ef4ac7030a6c6d49482fcc1dd27d0f8657
willswats <willswats@users.noreply.github.com>
2024-02-28 07:18:13 +0000

    Remove mullvad-gui for mullvad

--- modules/utilities/mullvad-vpn/home.nix
+++ modules/utilities/mullvad-vpn/home.nix
@@ -2,21 +2,6 @@
 
 {
   home.packages = with pkgs; [
-    mullvad-vpn
+    mullvad
   ];
-
-  xdg.configFile."Mullvad VPN/gui_settings.json" = {
-    text = ''
-      {
-           "preferredLocale": "system",
-           "autoConnect": false,
-           "enableSystemNotifications": true,
-           "monochromaticIcon": true,
-           "startMinimized": true,
-           "unpinnedWindow": true,
-           "browsedForSplitTunnelingApplications": [],
-           "changelogDisplayedForVersion": "2023.6"
-         }
-    '';
-  };
 }
