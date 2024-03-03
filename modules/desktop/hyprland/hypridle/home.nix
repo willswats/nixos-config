cc639935ac6f5895c372a0c937b3cb1e73bd3bc3
willswats <willswats@users.noreply.github.com>
2024-03-03 15:26:02 +0000

    Revert "Add dpms audio check to hypridle"
    This reverts commit e51951190ce8b0ec5632332ef298e6fd5e5b4335.

--- modules/desktop/hyprland/hypridle/home.nix
+++ modules/desktop/hyprland/hypridle/home.nix
@@ -5,15 +5,6 @@
     let
       swaylock = "${pkgs.swaylock-effects}/bin/swaylock";
       hyprctl = "${pkgs.hyprland}/bin/hyprctl";
-      dpmsOff = pkgs.writeShellScript "dpmsOff" ''
-        # This counts the number of running audio sources as lines.
-        # For some reason there is always two running audio sources.
-        # This checks if LINES is less than 3.
-        LINES=$(pw-cli i all 2>&1 | rg running | wc -l)
-        if [ "$LINES" -lt 3 ]; then
-          ${hyprctl} dispatch dpms off
-        fi
-      '';
     in
     {
       enable = true;
@@ -24,7 +15,7 @@
         }
         {
           timeout = 600;
-          onTimeout = dpmsOff.outPath;
+          onTimeout = "${hyprctl} dispatch dpms off";
           onResume = "${hyprctl} dispatch dpms on";
         }
       ];
