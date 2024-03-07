920c890eb9ccea1940f43e32266c0b035a67343d
willswats <willswats@users.noreply.github.com>
2024-03-07 10:17:36 +0000

    hypridle prevent lock when audio streams active

--- modules/desktop/hyprland/hypridle/home.nix
+++ modules/desktop/hyprland/hypridle/home.nix
@@ -5,17 +5,41 @@
     let
       swaylock = "${pkgs.swaylock-effects}/bin/swaylock";
       hyprctl = "${pkgs.hyprland}/bin/hyprctl";
+      wpctl = "${pkgs.wireplumber}/bin/wpctl";
+      rg = "${pkgs.ripgrep}/bin/rg";
+      # Both of these scripts check if there are no audio streams with "active"
+      # (there will be active audio streams when there is no audio playing, as long as a program is open that outputs audio)
+      # Both of these scripts could be replaced, if this issues gets closed with a PR: https://github.com/hyprwm/hypridle/issues/27
+      streamsActive = ''
+        streams_active() {
+          ${wpctl} status | ${rg} "active"
+        }
+      '';
+      lock = pkgs.writeShellScript "lock" ''
+        ${streamsActive}
+
+        if ! streams_active; then 
+          ${swaylock}
+        fi
+      '';
+      dpmsOff = pkgs.writeShellScript "dpmsOff" ''
+        ${streamsActive}
+
+        if ! streams_active; then 
+          ${hyprctl} dispatch dpms off
+        fi
+      '';
     in
     {
       enable = true;
       listeners = [
         {
           timeout = 1800;
-          onTimeout = swaylock;
+          onTimeout = lock.outPath;
         }
         {
           timeout = 600;
-          onTimeout = "${hyprctl} dispatch dpms off";
+          onTimeout = dpmsOff.outPath;
           onResume = "${hyprctl} dispatch dpms on";
         }
       ];
