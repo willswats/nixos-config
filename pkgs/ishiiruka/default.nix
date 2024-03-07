3b1e822b3f55838473176410681bdb71c1e3703f
willswats <willswats@users.noreply.github.com>
2024-03-07 13:24:40 +0000

    Update ishiiruka

--- pkgs/ishiiruka/default.nix
+++ pkgs/ishiiruka/default.nix
@@ -2,11 +2,11 @@
 
 appimageTools.wrapType2 rec {
   pname = "Ishiiruka";
-  version = "v3.3.1";
+  version = "v3.4.0";
 
   src = fetchurl {
     url = "https://github.com/project-slippi/Ishiiruka/releases/download/${version}/Slippi_Online-x86_64.AppImage";
-    sha256 = "sha256-XX02DcPW/lwExI6ZtxQHnyttkeA9BEy5ERH0g/NtEZY=";
+    sha256 = "086x9xma75qln08rs28n4m2krcs4w1jcb7jg5hiyslmbanyr06m6";
   };
 
   extraPkgs = pkgs: with pkgs;  [ gmp mpg123 libmpg123 ];
