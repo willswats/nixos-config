ff6289968764f34f6a3048775551ed87176e3421
willswats <willswats@users.noreply.github.com>
2024-03-07 23:03:08 +0000

    Add pcsx2 core

--- modules/games/retroarch/home.nix
+++ modules/games/retroarch/home.nix
@@ -13,6 +13,7 @@
           bsnes # - Nintendo - SNES / SFC (bsnes)
           blastem # Sega - Mega Drive - Genesis (BlastEm)
           swanstation # Sony - Playstation (SwanStation)
+          pcsx2 # Sony - Playstation 2 (LRPS2)
           desmume # Nintendo - DS (DeSmuME)
           citra # Nintendo - 3DS (Citra)
         ];
--- non-declarative/docs/games/retroarch.md
+++ non-declarative/docs/games/retroarch.md
@@ -2,4 +2,10 @@
 
 ## Add BIOS
 
+### PS1
+
 Add PS1 BIOS files (stored in my personal drive) to `~/.config/retroarch/system`.
+
+### PS2
+
+Add PS2 BIOS files (stored in my personal drive) to `~/.config/retroarch/system/pcsx2/bios`
