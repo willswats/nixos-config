5a78e53a5bed1ec4c5d99eabe1af01a3bee0b90f
willswats <willswats@users.noreply.github.com>
2024-03-11 22:21:52 +0000

    Change kitty settings

--- modules/utilities/kitty/home.nix
+++ modules/utilities/kitty/home.nix
@@ -20,12 +20,12 @@ in
       confirm_os_window_close = 0;
       window_padding_width = 0;
       window_margin_width = 0;
-      placement_strategy = "top-left";
-      tab_bar_min_tabs = 2;
       tab_bar_edge = "top";
       tab_bar_style = "separator";
       tab_separator = "|";
-      tab_title_template = "{title}{' :{}:'.format(num_windows) if num_windows > 1 else ''}";
+    };
+    keybindings = {
+      "ctrl+shift+t" = "new_tab_with_cwd";
     };
   };
 
