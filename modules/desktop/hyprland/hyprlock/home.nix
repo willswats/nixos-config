6f3af0415982d900dc0a7e16dd06b69707782f74
willswats <willswats@users.noreply.github.com>
2024-03-16 17:36:00 +0000

    Add to hyprlock config

--- modules/desktop/hyprland/hyprlock/home.nix
+++ modules/desktop/hyprland/hyprlock/home.nix
@@ -4,24 +4,30 @@
   programs.hyprlock =
     let
       fontName = globals.font.name;
+      wallpaper = globals.wallpaper;
 
       monitorCenter = host.monitors.center;
 
       blue = "rgb(137, 180, 250)";
       text = "rgb(205, 214, 244)";
       base = "rgb(30, 30, 46)";
+      peach = "rgb(250,179,135)";
+      red = "rgb(243,139,168)";
     in
     {
       enable = true;
 
       general = {
         no_fade_in = true;
+        no_fade_out = true;
       };
 
       backgrounds = [{
         monitor = "";
-        path = "";
+        path = "${wallpaper}";
         color = base;
+
+        blur_passes = 1;
       }];
 
       input-fields = [{
@@ -38,24 +44,33 @@
         inner_color = base;
         font_color = text;
 
+        check_color = peach;
+        fail_color = red;
+        fail_text = "";
+        fail_transition = 0;
+        capslock_color = peach;
+
         fade_on_empty = false;
+        fade_timeout = 0;
         placeholder_text = "";
+        rounding = 0;
 
         dots_spacing = 0.3;
         dots_center = true;
+        dots_rounding = 0;
       }];
 
       labels = [
         {
           monitor = monitorCenter;
           text = "$TIME";
-          font_size = 50;
-          font_family = fontName;
+          font_size = 60;
+          font_family = "${fontName} Bold";
           color = blue;
 
           position = {
             x = 0;
-            y = 100;
+            y = 80;
           };
 
           valign = "center";
@@ -64,13 +79,13 @@
         {
           monitor = monitorCenter;
           text = "cmd[update:1000] echo \"$(date +'%a %d %b')\"";
-          font_size = 25;
+          font_size = 20;
           font_family = fontName;
           color = blue;
 
           position = {
             x = 0;
-            y = 20;
+            y = 10;
           };
 
           valign = "center";
