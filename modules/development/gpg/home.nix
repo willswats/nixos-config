3f156941b2c843f883a8241d35e51de91ae37be3
willswats <willswats@users.noreply.github.com>
2024-03-18 19:25:55 +0000

    Update flake

--- modules/development/gpg/home.nix
+++ modules/development/gpg/home.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.gpg = {
@@ -14,6 +14,6 @@
     maxCacheTtl = 86400; # 86400 seconds - 24 hours
     defaultCacheTtlSsh = 86400; # 86400 seconds - 24 hours
     maxCacheTtlSsh = 86400; # 86400 seconds - 24 hours
-    pinentryFlavor = "qt";
+    pinentryPackage = pkgs.pinentry-gtk2;
   };
 }
