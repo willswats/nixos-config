5cafbe1ff0b55aefa35a973318c8b7941566ce5c
willswats <willswats@users.noreply.github.com>
2024-03-24 20:56:08 +0000

    Enable gpg signing

--- modules/development/git/home.nix
+++ modules/development/git/home.nix
@@ -6,12 +6,10 @@ in {
     enable = true;
     userName = "willswats";
     userEmail = email;
-    # Deleting a GPG key in GitHub that is no longer used causes all of the commits signed by the key to become "Unverified", therefore, 
-    # only uncomment this when you have a good way of transferring a GPG key between computers.
-    # signing = {
-    #   key = email;
-    #   signByDefault = true;
-    # };
+    signing = {
+      key = email;
+      signByDefault = true;
+    };
     extraConfig = { init.defaultBranch = "main"; };
   };
 }
