a91dc1cdc5f2ccd3b5647956144296927735d474
willswats <willswats@users.noreply.github.com>
2024-04-03 23:45:24 +0100

    chore: switch back to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/staging-next";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
