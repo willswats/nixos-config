e49ba49560e8a98482b70b1aec332e2db07beabd
willswats <willswats@users.noreply.github.com>
2024-04-04 17:23:04 +0100

    chore: remove yuzu

--- pkgs/yuzu/default.nix
+++
@@ -1,8 +0,0 @@
-{ appimageTools, ... }:
-
-appimageTools.wrapType2
-{
-  pname = "Yuzu";
-  version = "4176";
-  src = /home/will/Dropbox/Games/Emulation/Emulators/Yuzu/Linux-Yuzu-EA-4176.AppImage;
-}
