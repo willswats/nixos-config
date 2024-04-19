5eeb6d841dbedbdcc6dd3a4df1754759248f8a11
willswats <willswats@users.noreply.github.com>
2024-04-19 16:12:57 +0100

    chore: update flake, options, switch hyprshade to nixpkgs

--- modules/development/neovim/plugins/toggleterm.nix
+++ modules/development/neovim/plugins/toggleterm.nix
@@ -82,11 +82,13 @@
 
     plugins.toggleterm = {
       enable = true;
-      autochdir =
-        true; # When neovim changes it current directory the terminal will change it’s own when next it’s opened
-      openMapping = "<c-t>"; # Mapping to open the terminal
-      direction = "float"; # Floating terminal
-      floatOpts = { border = "single"; }; # Floating terminal options
+      settings = {
+        autochdir =
+          true; # When neovim changes it current directory the terminal will change it’s own when next it’s opened
+        openMapping = "<c-t>"; # Mapping to open the terminal
+        direction = "float"; # Floating terminal
+        floatOpts = { border = "single"; }; # Floating terminal options
+      };
     };
     extraConfigLua = ''
       local toggleterm_terminal = require("toggleterm.terminal")
