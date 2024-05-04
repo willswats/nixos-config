d642d7725778b50465f56275035ba75af13aa7e5
willswats <willswats@users.noreply.github.com>
2024-05-04 19:48:01 +0100

    feat: change pinetry

--- modules/development/gpg/home.nix
+++ modules/development/gpg/home.nix
@@ -14,6 +14,6 @@
     maxCacheTtl = 86400; # 86400 seconds - 24 hours
     defaultCacheTtlSsh = 86400; # 86400 seconds - 24 hours
     maxCacheTtlSsh = 86400; # 86400 seconds - 24 hours
-    pinentryPackage = pkgs.pinentry-gtk2;
+    pinentryPackage = pkgs.pinentry-gnome3;
   };
 }
