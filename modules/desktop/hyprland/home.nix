b1bf6b1b7ad5a84d912f21f7ea00895200da4001
willswats <willswats@users.noreply.github.com>
2024-05-16 03:48:12 +0100

    fix: remove duplicate start of hypridle again

--- modules/desktop/hyprland/home.nix
+++ modules/desktop/hyprland/home.nix
@@ -38,7 +38,6 @@
 
       # hyprctl = "${inputs.hyprland.packages.${pkgs.system}.hyprland}/bin/hyprctl";
       hyprctl = "${pkgs.hyprland}/bin/hyprctl";
-      hypridle = "${inputs.hypridle.packages.${pkgs.system}.hypridle}/bin/hypridle";
       hyprlock = "${inputs.hyprlock.packages.${pkgs.system}.hyprlock}/bin/hyprlock";
       hyprshade = "${pkgs.hyprshade}/bin/hyprshade";
 
@@ -196,7 +195,6 @@
           "${swww-daemon}"
           "${swww} img --transition-type none ${wallpaper}"
           # Daemons
-          "${hypridle}"
           "${playerctld}" # The home manager service doesn't seem to start playerctld
           "${lxpolkit}"
           # Applets
