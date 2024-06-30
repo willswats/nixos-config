4c62e51e9176daef99e190c4dd48da67b85a1e8e
willswats <william@williamwatson.dev>
2024-06-30 23:43:22 +0100

    feat: add sway-audio-idle-inhibit

--- modules/desktop/sway/swayidle/home.nix
+++ modules/desktop/sway/swayidle/home.nix
@@ -1,28 +1,40 @@
 { pkgs, ... }:
 
-let
-  swaylock = "${pkgs.swaylock-effects}/bin/swaylock";
-  swaymsg = "${pkgs.sway}/bin/swaymsg";
-in
 {
-  services = {
-    swayidle = {
-      enable = true;
-      timeouts = [
-        {
-          timeout = 1800; # 1800 seconds = 30 minutes
-          command = swaylock;
-        }
-        {
-          timeout = 600; # 600 seconds = 10 minutes
-          command = "${swaymsg} 'output * power off'";
-          resumeCommand = "${swaymsg} 'output * power on'";
-        }
-      ];
-      events = [
-        { event = "before-sleep"; command = swaylock; }
-        { event = "lock"; command = swaylock; }
-      ];
+  wayland.windowManager.sway.config.startup =
+    let
+      sway-audio-idle-inhibit = "${pkgs.sway-audio-idle-inhibit}/bin/sway-audio-idle-inhibit";
+    in
+    [
+      {
+        command = "${sway-audio-idle-inhibit}";
+        always = false;
+      }
+    ];
+
+  services =
+    let
+      swaylock = "${pkgs.swaylock-effects}/bin/swaylock";
+      swaymsg = "${pkgs.sway}/bin/swaymsg";
+    in
+    {
+      swayidle = {
+        enable = true;
+        timeouts = [
+          {
+            timeout = 1800; # 1800 seconds = 30 minutes
+            command = swaylock;
+          }
+          {
+            timeout = 600; # 600 seconds = 10 minutes
+            command = "${swaymsg} 'output * power off'";
+            resumeCommand = "${swaymsg} 'output * power on'";
+          }
+        ];
+        events = [
+          { event = "before-sleep"; command = swaylock; }
+          { event = "lock"; command = swaylock; }
+        ];
+      };
     };
-  };
 }
