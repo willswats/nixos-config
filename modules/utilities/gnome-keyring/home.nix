b14c5feec4956a7c327c6e386f211b0bc0e9ab0f
willswats <william@williamwatson.dev>
2024-07-13 22:03:48 +0100

    feat: update flake and pkg names

--- modules/utilities/gnome-keyring/home.nix
+++ modules/utilities/gnome-keyring/home.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ gnome.seahorse gcr ];
+  home.packages = with pkgs; [ seahorse gcr ];
 }
--- modules/utilities/pcmanfm/home.nix
+++ modules/utilities/pcmanfm/home.nix
@@ -3,7 +3,7 @@
 {
   home.packages = with pkgs; [
     pcmanfm
-    gnome.file-roller
+    file-roller
   ];
 
   xdg.mimeApps = {
