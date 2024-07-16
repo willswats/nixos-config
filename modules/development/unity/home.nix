f3023cb131baaeb9fe303a928bc4bc5e155d8ee9
willswats <william@williamwatson.dev>
2024-07-16 17:50:08 +0100

    refactor: add packages to unity

--- modules/development/unity/home.nix
+++ modules/development/unity/home.nix
@@ -1,9 +1,14 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    unityhub
-    ffmpeg # Required for converting mp3 to wav by Unity
-    mono
+  home.packages = [
+    (pkgs.unityhub.override {
+      # Found out how to do this from https://huantian.dev/blog/unity3d-rider-nixos/
+      extraPkgs = fhsPkgs: [
+        # Required for building
+        fhsPkgs.python3
+        fhsPkgs.ffmpeg
+      ];
+    })
   ];
 }
