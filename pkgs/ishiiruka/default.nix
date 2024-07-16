10a16fd1b9edd80df139815dcf6174c1a0c70c29
willswats <william@williamwatson.dev>
2024-07-16 01:17:34 +0100

    chore: update to ishiiruka to v3.4.1
    s

--- pkgs/ishiiruka/default.nix
+++ pkgs/ishiiruka/default.nix
@@ -2,11 +2,11 @@
 
 appimageTools.wrapType2 rec {
   pname = "Ishiiruka";
-  version = "v3.4.0";
+  version = "v3.4.1";
 
   src = fetchurl {
     url = "https://github.com/project-slippi/Ishiiruka/releases/download/${version}/Slippi_Online-x86_64.AppImage";
-    sha256 = "086x9xma75qln08rs28n4m2krcs4w1jcb7jg5hiyslmbanyr06m6";
+    sha256 = "sha256-Ns0yhnb2H0wDj+vUtjSgujpVV8GjKrKSf+OoaCuXIKA=";
   };
 
   extraPkgs = pkgs: with pkgs;  [ gmp mpg123 libmpg123 ];
