438503606734b19eff84c13caaee5b3db1c71a0a
willswats <william@williamwatson.dev>
2024-07-19 14:30:34 +0100

    feat(nvim): set show message inc-rename

--- modules/development/neovim/plugins/inc-rename.nix
+++ modules/development/neovim/plugins/inc-rename.nix
@@ -3,12 +3,15 @@
 {
   programs.nixvim = {
     plugins = {
-      inc-rename.enable = true;
+      inc-rename = {
+        enable = true;
+        showMessage = true;
+      };
     };
 
     # Fill in the word under the cursor 
     extraConfigLua = ''
-      vim.keymap.set("n", "<leader>lr", function()
+      vim.keymap.set("n", "<leader>R", function()
         return ":IncRename " .. vim.fn.expand("<cword>")
       end, { expr = true, desc = "Rename" })
     '';
