4932605793537d99525075a371e55154c346fb45
willswats <william@williamwatson.dev>
2024-08-19 22:05:02 +0100

    fix(nvim): bufferline

--- modules/development/neovim/plugins/bufferline.nix
+++ modules/development/neovim/plugins/bufferline.nix
@@ -73,19 +73,12 @@ in {
 
     plugins.bufferline = {
       enable = true;
-      settings = {
-        closeCommand = "confirm bdelete %d";
-        rightMouseCommand = "confirm bdelete %d";
-        showBufferCloseIcons = false;
-        alwaysShowBufferline = false;
-        indicator.icon = icons.ui.BoldLineLeft;
-        bufferCloseIcon = icons.ui.Close;
-        modifiedIcon = icons.ui.Circle;
-        closeIcon = icons.ui.BoldClose;
-        leftTruncMarker = icons.ui.ArrowCircleLeft;
-        rightTruncMarker = icons.ui.ArrowCircleRight;
+      settings.options = {
+        always_show_bufferline = false;
+        buffer_close_icon = icons.ui.Close;
+        close_icon = icons.ui.BoldClose;
         diagnostics = "nvim_lsp";
-        diagnosticsIndicator = ''
+        diagnostics_indicator = ''
           function(count, level)
             local icon = ""
             if level:match("error") then
@@ -98,6 +91,14 @@ in {
             return " " .. icon .. " " .. count
           end
         '';
+        indicator.icon = icons.ui.BoldLineLeft;
+        left_trunc_marker = icons.ui.ArrowCircleLeft;
+        modified_icon = icons.ui.Circle;
+        right_trunc_Marker = icons.ui.ArrowCircleRight;
+        close_command = "confirm bdelete %d";
+        right_mouse_command = "confirm bdelete %d";
+        show_buffer_close_icons = false;
+        show_tab_indicators = false;
       };
     };
   };
