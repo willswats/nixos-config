4f3ac4fac76fcfdc4701c146e3af4c098729a36d
willswats <william@williamwatson.dev>
2024-08-26 17:06:43 +0100

    feat: remove gaps

--- modules/desktop/hyprland/home.nix
+++ modules/desktop/hyprland/home.nix
@@ -78,8 +78,8 @@
       package = inputs.hyprland.packages.${pkgs.system}.default;
       settings = {
         general = {
-          gaps_in = 5;
-          gaps_out = 10;
+          gaps_in = 0;
+          gaps_out = 0;
           border_size = 2;
           "col.active_border" = blue;
           "col.inactive_border" = overlay0;
