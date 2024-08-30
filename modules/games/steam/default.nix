0307f06f54ae4a3a0c769b3aba5c9f33e4dfbf28
willswats <william@williamwatson.dev>
2024-08-30 22:50:16 +0100

    chore: update comment

--- modules/games/steam/default.nix
+++ modules/games/steam/default.nix
@@ -10,7 +10,7 @@
     # ];
   };
 
-  # TF2 is currently broken on Linux and this is needed to fix it.
+  # Certain source games are currently broken on Linux and this is needed to fix it.
   # Issue: https://github.com/ValveSoftware/Source-1-Games/issues/5043
   # Fix: https://github.com/NixOS/nixpkgs/issues/271483#issuecomment-1838055011
   environment.systemPackages = with pkgs; [
