161744a656e15d6a7a6f2885db16e3d1b8ba334f
willswats <william@williamwatson.dev>
2024-08-30 10:19:01 +0100

    feat: add pipewire low latency

--- modules/system/pipewire/default.nix
+++ modules/system/pipewire/default.nix
@@ -9,5 +9,14 @@
     pulse.enable = true;
     jack.enable = true;
     wireplumber.enable = true;
+    # For low latency - keep increasing the quant value until you get no crackles
+    extraConfig.pipewire."92-low-latency" = {
+      context.properties = {
+        default.clock.rate = 48000;
+        default.clock.quantum = 32;
+        default.clock.min-quantum = 32;
+        default.clock.max-quantum = 32;
+      };
+    };
   };
 }
