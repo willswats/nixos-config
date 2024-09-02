34e1afaa33fed2195c7a82034b5f4d1a432b414b
willswats <william@williamwatson.dev>
2024-09-02 01:21:48 +0100

    feat: add back gamescope to steam

--- modules/games/steam/default.nix
+++ modules/games/steam/default.nix
@@ -2,12 +2,42 @@
 
 
 {
+
   programs.steam = {
     enable = true;
+    gamescopeSession = {
+      enable = true;
+      args = [
+        "-w 1920"
+        "-h 1080"
+        "-W 1920"
+        "-H 1080"
+        "-f" # Fullscreen by default
+        "-e" # Enable steam integration (fix mouse issues on certain games)
+      ];
+    };
     # Not using this, because when proton-ge-bin updates, all games set with it are reset to no proton
     # extraCompatPackages = with pkgs; [
     #   proton-ge-bin
     # ];
+  };
+
+  # Fix gamescope on steam https://github.com/ValveSoftware/gamescope/issues/905#issuecomment-1660112668
+  nixpkgs.config.packageOverrides = pkgs: {
+    steam = pkgs.steam.override {
+      extraPkgs = pkgs: with pkgs; [
+        keyutils
+        libkrb5
+        libpng
+        libpulseaudio
+        libvorbis
+        stdenv.cc.cc.lib
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXinerama
+        xorg.libXScrnSaver
+      ];
+    };
   };
 
   # Certain source games are currently broken on Linux and this is needed to fix it.
