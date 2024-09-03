140f1d4f545789bd6608efbc4549732744b719e4
willswats <william@williamwatson.dev>
2024-09-03 03:57:19 +0100

    refactor: increase quantum

--- modules/system/pipewire/default.nix
+++ modules/system/pipewire/default.nix
@@ -13,7 +13,7 @@
     extraConfig.pipewire."92-low-latency" = {
       context.properties = {
         default.clock.rate = 48000;
-        default.clock.quantum = 32;
+        default.clock.quantum = 64;
         default.clock.min-quantum = 32;
         default.clock.max-quantum = 32;
       };
