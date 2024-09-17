986c75e61d5478aa160ebee3a84f3cb5707f9daf
willswats <william@williamwatson.dev>
2024-09-17 12:07:05 +0100

    feat: attempt fix pipewire crackle

--- modules/system/pipewire/default.nix
+++ modules/system/pipewire/default.nix
@@ -1,6 +1,7 @@
 { ... }:
 
 {
+  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -13,9 +14,9 @@
     extraConfig.pipewire."92-low-latency" = {
       context.properties = {
         default.clock.rate = 48000;
-        default.clock.quantum = 64;
-        default.clock.min-quantum = 32;
-        default.clock.max-quantum = 32;
+        default.clock.quantum = 1024;
+        default.clock.min-quantum = 1024;
+        default.clock.max-quantum = 1024;
       };
     };
   };
