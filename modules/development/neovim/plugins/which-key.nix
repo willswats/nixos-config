d81078189ba4dfeb9a2b38a5a760c2b41f65c0fb
willswats <william@williamwatson.dev>
2024-10-18 19:17:39 +0100

    refactor(nvim): switch back to nixvim for which-key

--- modules/development/neovim/plugins/which-key.nix
+++ modules/development/neovim/plugins/which-key.nix
@@ -1,62 +1,47 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   programs.nixvim = {
-    # plugins.which-key = {
-    # enable = true;
-    #   settings.spec = [
-    #     {
-    #       __unkeyed-1 = "<leader>b";
-    #       desc = "Buffers";
-    #     }
-    #     {
-    #       __unkeyed-1 = "<leader>d";
-    #       desc = "Debug";
-    #     }
-    #     {
-    #       __unkeyed-1 = "<leader>f";
-    #       desc = "Find";
-    #     }
-    #     {
-    #       __unkeyed-1 = "<leader>g";
-    #       desc = "Git";
-    #     }
-    #     {
-    #       __unkeyed-1 = "<leader>l";
-    #       desc = "LSP";
-    #     }
-    #     {
-    #       __unkeyed-1 = "<leader>m";
-    #       desc = "Multicursors";
-    #     }
-    #     {
-    #       __unkeyed-1 = "<leader>t";
-    #       desc = "Terminal";
-    #     }
-    #   ];
-    # };
-    # Not using the NixVim config for which-key until the module has been updated to have more which-key v3 options (can't disable icons or set preset)
-    extraPlugins = with pkgs.vimPlugins;[ which-key-nvim ];
-    extraConfigLua = ''
-      local wk = require("which-key")
-
-      wk.setup({
-        preset = "helix",
+    plugins.which-key = {
+      enable = true;
+      settings = {
+        delay = 0;
+        preset = "helix";
         icons = {
-          mappings = false,
-          rules = false,
-        }
-      })
-
-      wk.add({
-        { "<leader>b", group = "Buffers" },
-        { "<leader>d", group = "Debug" },
-        { "<leader>f", group = "Find" },
-        { "<leader>g", group = "Git" },
-        { "<leader>l", group = "LSP" },
-        { "<leader>m", group = "Multicursors" },
-        { "<leader>t", group = "Terminal" },
-      })
-    '';
+          mappings = false;
+          rules = false;
+        };
+        spec = [
+          {
+            __unkeyed-1 = "<leader>b";
+            desc = "Buffers";
+          }
+          {
+            __unkeyed-1 = "<leader>d";
+            desc = "Debug";
+          }
+          {
+            __unkeyed-1 = "<leader>f";
+            desc = "Find";
+          }
+          {
+            __unkeyed-1 = "<leader>g";
+            desc = "Git";
+          }
+          {
+            __unkeyed-1 = "<leader>l";
+            desc = "LSP";
+          }
+          {
+            __unkeyed-1 = "<leader>m";
+            desc = "Multicursors";
+          }
+          {
+            __unkeyed-1 = "<leader>t";
+            desc = "Terminal";
+          }
+        ];
+      };
+    };
   };
 }
