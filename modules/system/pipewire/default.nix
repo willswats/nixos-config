ff96f085c5c38fc8ea0fc001d644ccdbeb1770d0
willswats <william@williamwatson.dev>
2024-11-08 17:18:22 +0000

    feat(pipewire): double quantum
    fix audio crackle osu + streamlink

--- modules/system/pipewire/default.nix
+++ modules/system/pipewire/default.nix
@@ -14,7 +14,7 @@
     extraConfig.pipewire."92-low-latency" = {
       context.properties = {
         default.clock.rate = 48000;
-        default.clock.quantum = 1024;
+        default.clock.quantum = 2048;
         default.clock.min-quantum = 1024;
         default.clock.max-quantum = 1024;
       };
