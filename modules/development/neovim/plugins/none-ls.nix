fe577253da13c57ecc9ce90e14c542c543a8ce55
willswats <william@williamwatson.dev>
2024-12-01 01:43:47 +0000

    feat(nvim): add to sqlfluff settings

--- modules/development/neovim/plugins/none-ls.nix
+++ modules/development/neovim/plugins/none-ls.nix
@@ -3,29 +3,42 @@
 {
   programs.nixvim.plugins.none-ls = {
     enable = true;
-    sources = {
-      formatting = {
-        prettier = {
-          enable = true; # Web
-          disableTsServerFormatter = true;
+    sources =
+      let
+        # Must be specified here as the formatter does not seem to read the settings from
+        # the .sqlfluff file for some reason
+        sqlfluffSettings = ''
+          { 
+              extra_args = { 
+                  "--dialect", "postgres", 
+                  "--exclude-rules", "ST07, LT02" 
+              } 
+          }
+        '';
+      in
+      {
+        formatting = {
+          prettier = {
+            enable = true; # Web
+            disableTsServerFormatter = true;
+          };
+          nixpkgs_fmt.enable = true; # Nix
+          shfmt.enable = true; # Bash
+          sqlfluff = {
+            enable = true; # SQL
+            settings = sqlfluffSettings;
+          };
         };
-        nixpkgs_fmt.enable = true; # Nix
-        shfmt.enable = true; # Bash
-        sqlfluff = {
-          enable = true; # SQL
-          settings = ''{ extra_args = { "--dialect", "postgres" } }'';
+        diagnostics = {
+          sqlfluff = {
+            enable = true; # SQL
+            settings = sqlfluffSettings;
+          };
+          markdownlint = {
+            enable = true; # Markdown
+            settings = ''{ extra_args = { "--disable", "MD013" } }'';
+          };
         };
       };
-      diagnostics = {
-        sqlfluff = {
-          enable = true; # SQL
-          settings = ''{ extra_args = { "--dialect", "postgres" } }'';
-        };
-        markdownlint = {
-          enable = true; # Markdown
-          settings = ''{ extra_args = { "--disable", "MD013" } }'';
-        };
-      };
-    };
   };
 }
