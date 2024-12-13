01c998406b6c9a5a1a5e229f17a0bc0f55aa1091
willswats <william@williamwatson.dev>
2024-12-13 12:57:13 +0000

    feat: switch to amdvlk drivers

--- modules/system/amd/default.nix
+++ modules/system/amd/default.nix
@@ -1,9 +1,17 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   boot.initrd.kernelModules = [ "amdgpu" ];
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
+    extraPackages = with pkgs; [ amdvlk ];
+    extraPackages32 = with pkgs; [
+      driversi686Linux.amdvlk
+    ];
   };
+
+  # "RADV" or "AMDVLK"
+  # Default is AMDVLK if installed
+  environment.variables.AMD_VULKAN_ICD = "AMDVLK";
 }
