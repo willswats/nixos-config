5cfc31b93040fb4c2b0453112414acb801a312e0
willswats <william@williamwatson.dev>
2025-03-27 13:44:12 +0000

    feat: add virtualbox

---
+++ modules/utilities/virtualbox/default.nix
@@ -0,0 +1,11 @@
+{ globals, ... }:
+
+{
+  virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.guest.enable = true;
+  virtualisation.virtualbox.guest.dragAndDrop = true;
+  users.extraGroups.vboxusers.members = [ globals.user ];
+
+  # Fix: https://github.com/NixOS/nixpkgs/issues/363887#issuecomment-2536693220
+  boot.kernelParams = [ "kvm.enable_virt_at_load=0" ];
+}
