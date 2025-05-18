f62fadf32a997a916deecf5baa8fec1dfeca4277
willswats <william@williamwatson.dev>
2025-05-18 01:56:52 +0100

    feat: add css cfg

---
+++ modules/games/steam/css/home.nix
@@ -0,0 +1,30 @@
+{ ... }:
+
+{
+  home.file.".steam/steam/steamapps/common/Counter-Strike Source/cstrike/cfg/autoexec.cfg".text =
+    ''
+      echo "autoexec.cfg executed"
+
+      # -- UNBIND DEFAULTS --
+
+      unbind mwheelup
+      unbind mwheeldown
+
+      # -- BINDS --
+
+      bind mwheeldown +jump
+      bind mwheelup +jump
+      bind mouse5 +left
+      bind mouse4 +right
+
+      bind semicolon "say !r"
+      bind ralt sm_teleport
+
+      # -- SETTINGS --
+
+      # Game
+      cl_yawspeed 230
+ 
+      host_writeconfig
+    '';
+}
--- modules/games/steam/home.nix
+++ modules/games/steam/home.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ./cs2/home.nix
+    ./css/home.nix
     ./tf2/home.nix
     ./garrys-mod/home.nix
     ./half-life/home.nix
