28a6833627585258a3da6ae28031b4f4054ccad5
willswats <william@williamwatson.dev>
2025-06-17 23:13:23 +0100

    refactor: remove pipewire low latency

--- modules/system/pipewire/default.nix
+++ modules/system/pipewire/default.nix
@@ -10,38 +10,5 @@
     pulse.enable = true;
     jack.enable = true;
     wireplumber.enable = true;
-    # For low latency - keep increasing the quant value until you get no crackles
-    extraConfig = {
-      pipewire."92-low-latency" = {
-        context.properties = {
-          default.clock.rate = 48000;
-          default.clock.quantum = 32;
-          default.clock.min-quantum = 1024;
-          default.clock.max-quantum = 1024;
-        };
-      };
-      # Applications using the Pulse backend have a separate configuration. 
-      # The default minimum value  is 1024, so it needs to be tweaked if low-latency audio is desired.  
-      # As a general rule, the values in pipewire-pulse should not be lower than the ones in pipewire. 
-      pipewire-pulse."92-low-latency" = {
-        "context.properties" = [
-          {
-            name = "libpipewire-module-protocol-pulse";
-            args = { };
-          }
-        ];
-        "pulse.properties" = {
-          "pulse.min.req" = "32/48000";
-          "pulse.default.req" = "32/48000";
-          "pulse.max.req" = "32/48000";
-          "pulse.min.quantum" = "32/48000";
-          "pulse.max.quantum" = "32/48000";
-        };
-        "stream.properties" = {
-          "node.latency" = "32/48000";
-          "resample.quality" = 1;
-        };
-      };
-    };
   };
 }
