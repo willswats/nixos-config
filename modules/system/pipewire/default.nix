e9420e1d0d1ebd116cbeff8640378ec2328cd83f
willswats <william@williamwatson.dev>
2025-06-22 12:23:27 +0100

    feat: change pipewire values

--- modules/system/pipewire/default.nix
+++ modules/system/pipewire/default.nix
@@ -15,14 +15,14 @@
       pipewire."92-low-latency" = {
         context.properties = {
           default.clock.rate = 48000;
-          default.clock.quantum = 32;
-          default.clock.min-quantum = 1024;
+          default.clock.quantum = 800;
+          default.clock.min-quantum = 512;
           default.clock.max-quantum = 1024;
         };
       };
-      # Applications using the Pulse backend have a separate configuration. 
-      # The default minimum value  is 1024, so it needs to be tweaked if low-latency audio is desired.  
-      # As a general rule, the values in pipewire-pulse should not be lower than the ones in pipewire. 
+      # Applications using the Pulse backend have a separate configuration.
+      # The default minimum value  is 1024, so it needs to be tweaked if low-latency audio is desired.
+      # As a general rule, the values in pipewire-pulse should not be lower than the ones in pipewire.
       pipewire-pulse."92-low-latency" = {
         "context.properties" = [
           {
@@ -31,14 +31,14 @@
           }
         ];
         "pulse.properties" = {
-          "pulse.min.req" = "32/48000";
-          "pulse.default.req" = "32/48000";
-          "pulse.max.req" = "32/48000";
-          "pulse.min.quantum" = "32/48000";
-          "pulse.max.quantum" = "32/48000";
+          "pulse.min.req" = "256/48000";
+          "pulse.default.req" = "256/48000";
+          "pulse.max.req" = "256/48000";
+          "pulse.min.quantum" = "256/48000";
+          "pulse.max.quantum" = "256/48000";
         };
         "stream.properties" = {
-          "node.latency" = "32/48000";
+          "node.latency" = "256/48000";
           "resample.quality" = 1;
         };
       };
