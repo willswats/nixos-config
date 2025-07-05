d5dc01a9e19f581ff04bf8320ed589e1e564d847
willswats <william@williamwatson.dev>
2025-07-05 20:52:43 +0100

    reformat: firefox

--- modules/utilities/firefox/home.nix
+++ modules/utilities/firefox/home.nix
@@ -69,13 +69,13 @@
           "wikipedia".metaData.hidden = false;
           # Custom
           "Dictionary" = {
-            urls = [{ template = "https://www.dictionary.com/browse/{searchTerms}"; }];
+            urls = [ { template = "https://www.dictionary.com/browse/{searchTerms}"; } ];
             icon = "https://www.dictionary.com/94e56a525da4e9fe0cda.png";
             updateInterval = 24 * 60 * 60 * 1000; # every day
             definedAliases = [ "@d" ];
           };
           "Thesaurus" = {
-            urls = [{ template = "https://www.thesaurus.com/browse/{searchTerms}"; }];
+            urls = [ { template = "https://www.thesaurus.com/browse/{searchTerms}"; } ];
             icon = "https://www.thesaurus.com/0d297be7e698b98c9da8.png";
             updateInterval = 24 * 60 * 60 * 1000; # every day
             definedAliases = [ "@t" ];
@@ -119,12 +119,12 @@
             definedAliases = [ "@no" ];
           };
           "NixOS Wiki" = {
-            urls = [{ template = "https://wiki.nixos.org/w/index.php?search={searchTerms}"; }];
+            urls = [ { template = "https://wiki.nixos.org/w/index.php?search={searchTerms}"; } ];
             icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
             definedAliases = [ "@nw" ];
           };
           "MDN" = {
-            urls = [{ template = "https://developer.mozilla.org/en-US/search?q={searchTerms}"; }];
+            urls = [ { template = "https://developer.mozilla.org/en-US/search?q={searchTerms}"; } ];
             icon = "https://developer.mozilla.org/favicon-48x48.bc390275e955dacb2e65.png";
             updateInterval = 24 * 60 * 60 * 1000; # every day
             definedAliases = [ "@mdn" ];
