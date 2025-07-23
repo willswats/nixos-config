c1af6ef69c824a70d03b6cb06b9f34c62ce9a5a6
willswats <william@williamwatson.dev>
2025-07-23 01:00:53 +0100

    feat(bsp): add tf2

--- modules/games/steam/bsp-casefolding-workaround/home.nix
+++ modules/games/steam/bsp-casefolding-workaround/home.nix
@@ -2,12 +2,14 @@
 
 let
   homeDir = globals.directories.home;
+  steamDir = "${homeDir}/.local/share/Steam/steamapps/common";
 in
 {
   services.bsp-casefolding-workaround = {
     enable = true;
     watch_dirs = [
-      "${homeDir}/.local/share/Steam/steamapps/common/Counter-Strike Source/cstrike/download"
+      "${steamDir}/Counter-Strike Source/cstrike/download"
+      "${steamDir}/Team Fortress 2/tf/download"
     ];
   };
 }
