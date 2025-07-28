ca072490a4a7b628e814fe20ee1764f1a294adcb
willswats <william@williamwatson.dev>
2025-07-28 01:12:25 +0100

    feat: change mpd dir

--- modules/servers/mpd/home.nix
+++ modules/servers/mpd/home.nix
@@ -1,14 +1,18 @@
-{ ... }:
+{ host, ... }:
 
 {
-  services.mpd = {
-    enable = true;
-    musicDirectory = "/home/will/Downloads/Music";
-    extraConfig = ''
-      audio_output {
-        type "pipewire"
-        name "My PipeWire Output"
-      }
-    '';
-  };
+  services.mpd =
+    let
+      driveDir = host.directories.drive;
+    in
+    {
+      enable = true;
+      musicDirectory = "${driveDir}/Entertainment/Music";
+      extraConfig = ''
+        audio_output {
+          type "pipewire"
+          name "My PipeWire Output"
+        }
+      '';
+    };
 }
