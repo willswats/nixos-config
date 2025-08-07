d26f338fdcf47571dd4fc834175682e9f62f2466
willswats <william@williamwatson.dev>
2025-08-07 12:59:22 +0100

    feat: remove custom audio prevention hypridle

--- modules/desktop/hyprland/hypridle/home.nix
+++ modules/desktop/hyprland/hypridle/home.nix
@@ -5,29 +5,6 @@
     let
       hyprlock = "${pkgs.hyprlock}/bin/hyprlock";
       hyprctl = "${pkgs.hyprland}/bin/hyprctl";
-      wpctl = "${pkgs.wireplumber}/bin/wpctl";
-      rg = "${pkgs.ripgrep}/bin/rg";
-      # Both of these scripts check if there are no audio streams with "active"
-      # Both of these scripts could be replaced, if this issues gets closed with a PR: https://github.com/hyprwm/hypridle/issues/27
-      streamsActive = ''
-        streams_active() {
-          ${wpctl} status | ${rg} "active"
-        }
-      '';
-      lock = pkgs.writeShellScript "lock" ''
-        ${streamsActive}
-
-        if ! streams_active; then 
-          ${hyprlock}
-        fi
-      '';
-      dpmsOff = pkgs.writeShellScript "dpmsOff" ''
-        ${streamsActive}
-
-        if ! streams_active; then 
-          ${hyprctl} dispatch dpms off
-        fi
-      '';
     in
     {
       enable = true;
@@ -36,11 +13,11 @@
         listener = [
           {
             timeout = 1800;
-            on-timeout = lock.outPath;
+            on-timeout = hyprlock;
           }
           {
             timeout = 600;
-            on-timeout = dpmsOff.outPath;
+            on-timeout = "${hyprctl} dispatch dpms off";
             on-resume = "${hyprctl} dispatch dpms on";
           }
         ];
