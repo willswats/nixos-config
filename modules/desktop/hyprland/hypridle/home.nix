f88dfcb24a0a49962c096d4caa9c74978ecee542
willswats <william@williamwatson.dev>
2025-08-09 02:33:08 +0100

    Revert "feat: remove custom audio prevention hypridle"
    This reverts commit d26f338fdcf47571dd4fc834175682e9f62f2466.

--- modules/desktop/hyprland/hypridle/home.nix
+++ modules/desktop/hyprland/hypridle/home.nix
@@ -5,6 +5,29 @@
     let
       hyprlock = "${pkgs.hyprlock}/bin/hyprlock";
       hyprctl = "${pkgs.hyprland}/bin/hyprctl";
+      wpctl = "${pkgs.wireplumber}/bin/wpctl";
+      rg = "${pkgs.ripgrep}/bin/rg";
+      # Both of these scripts check if there are no audio streams with "active"
+      # Both of these scripts could be replaced, if this issues gets closed with a PR: https://github.com/hyprwm/hypridle/issues/27
+      streamsActive = ''
+        streams_active() {
+          ${wpctl} status | ${rg} "active"
+        }
+      '';
+      lock = pkgs.writeShellScript "lock" ''
+        ${streamsActive}
+
+        if ! streams_active; then 
+          ${hyprlock}
+        fi
+      '';
+      dpmsOff = pkgs.writeShellScript "dpmsOff" ''
+        ${streamsActive}
+
+        if ! streams_active; then 
+          ${hyprctl} dispatch dpms off
+        fi
+      '';
     in
     {
       enable = true;
@@ -13,11 +36,11 @@
         listener = [
           {
             timeout = 1800;
-            on-timeout = hyprlock;
+            on-timeout = lock.outPath;
           }
           {
             timeout = 600;
-            on-timeout = "${hyprctl} dispatch dpms off";
+            on-timeout = dpmsOff.outPath;
             on-resume = "${hyprctl} dispatch dpms on";
           }
         ];
