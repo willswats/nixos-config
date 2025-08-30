d219c6210eee8fab0f9a549e2ee907ef7d72b88e
willswats <william@williamwatson.dev>
2025-08-30 11:08:51 +0100

    chore: update flake, switch to autotiling-rs

--- modules/desktop/sway/home.nix
+++ modules/desktop/sway/home.nix
@@ -72,7 +72,7 @@
       lxpolkit = "${pkgs.lxde.lxsession}/bin/lxpolkit";
       mullvadGui = "${pkgs.mullvad-vpn}/bin/mullvad-gui";
       dropbox = "${pkgs.dropbox}/bin/dropbox";
-      autotiling = "${pkgs.autotiling}/bin/autotiling";
+      autotiling = "${pkgs.autotiling-rs}/bin/autotiling-rs";
 
       wpctl = "${pkgs.wireplumber}/bin/wpctl";
       playerctl = "${pkgs.playerctl}/bin/playerctl";
