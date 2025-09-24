dc1c0feb13b12ba39e0eeb43d71cc98ce57e87d3
willswats <william@williamwatson.dev>
2025-09-24 09:58:51 +0100

    chore: update bookmarks

--- modules/utilities/firefox/home.nix
+++ modules/utilities/firefox/home.nix
@@ -178,13 +178,13 @@
           "wikipedia".metaData.hidden = false;
           # Custom
           "Dictionary" = {
-            urls = [{ template = "https://www.dictionary.com/browse/{searchTerms}"; }];
+            urls = [ { template = "https://www.dictionary.com/browse/{searchTerms}"; } ];
             icon = "https://www.dictionary.com/94e56a525da4e9fe0cda.png";
             updateInterval = 24 * 60 * 60 * 1000; # every day
             definedAliases = [ "@d" ];
           };
           "Thesaurus" = {
-            urls = [{ template = "https://www.thesaurus.com/browse/{searchTerms}"; }];
+            urls = [ { template = "https://www.thesaurus.com/browse/{searchTerms}"; } ];
             icon = "https://www.thesaurus.com/0d297be7e698b98c9da8.png";
             updateInterval = 24 * 60 * 60 * 1000; # every day
             definedAliases = [ "@t" ];
@@ -228,12 +228,12 @@
             definedAliases = [ "@no" ];
           };
           "NixOS Wiki" = {
-            urls = [{ template = "https://wiki.nixos.org/w/index.php?search={searchTerms}"; }];
+            urls = [ { template = "https://wiki.nixos.org/w/index.php?search={searchTerms}"; } ];
             icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
             definedAliases = [ "@nw" ];
           };
           "MDN" = {
-            urls = [{ template = "https://developer.mozilla.org/en-US/search?q={searchTerms}"; }];
+            urls = [ { template = "https://developer.mozilla.org/en-US/search?q={searchTerms}"; } ];
             icon = "https://developer.mozilla.org/favicon-48x48.bc390275e955dacb2e65.png";
             updateInterval = 24 * 60 * 60 * 1000; # every day
             definedAliases = [ "@mdn" ];
@@ -586,6 +586,18 @@
                   {
                     name = "DCalc";
                     url = "https://soc.port.ac.uk/dcalc/";
+                  }
+                  {
+                    name = "Google Scholar";
+                    url = "https://scholar.google.co.uk/";
+                  }
+                  {
+                    name = "Scopus";
+                    url = "https://www.scopus.com/";
+                  }
+                  {
+                    name = "Ebsco";
+                    url = "https://research.ebsco.com/c/qgabbp/search";
                   }
                 ];
               }
