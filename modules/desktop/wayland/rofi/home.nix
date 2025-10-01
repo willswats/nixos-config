8a8d8bdfe3cadccab4541e02657d58c627856324
willswats <william@williamwatson.dev>
2025-10-01 22:29:26 +0100

    feat: add rofi font size

--- modules/desktop/wayland/rofi/home.nix
+++ modules/desktop/wayland/rofi/home.nix
@@ -1,10 +1,11 @@
-{ pkgs, ... }:
+{ pkgs, globals, ... }:
 
 {
   catppuccin.rofi.enable = true;
 
   programs.rofi = {
     enable = true;
+    font = "${globals.font.name} 14";
     terminal = "${pkgs.kitty}/bin/kitty";
     extraConfig = {
       show-icons = true;
