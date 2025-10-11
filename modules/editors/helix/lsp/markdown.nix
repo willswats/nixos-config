5dfa490f4a522caf90370e4cca20e7ed244df7f0
willswats <william@williamwatson.dev>
2025-10-11 15:41:45 +0100

    feat(helix): add markdownlint-cli

--- modules/editors/helix/lsp/markdown.nix
+++ modules/editors/helix/lsp/markdown.nix
@@ -5,6 +5,8 @@
   programs.helix.extraPackages = with pkgs; [
     marksman # MD Language server
     harper # Spell checker
+    efm-langserver # Wrapper lsp for markdownlint-cli
+    markdownlint-cli # Markdown linter
   ];
 
   xdg.configFile."helix/languages.toml".text = lib.mkAfter ''
@@ -15,11 +17,34 @@
     [language-server.harper-ls.config.harper-ls]   
     dialect = "British"
     
+    [language-server.efm]
+    command = "efm-langserver"
+    
     [[language]]
     name = "markdown"
     formatter = { command = 'prettier', args = ["--parser", "markdown"] }
-    language-servers = [ "marksman", "harper-ls" ]
+    language-servers = [ "marksman", "harper-ls", { name = "efm", only-features = [ "diagnostics" ] } ]
     auto-format = true
     soft-wrap.enable = true
+
+    
+  '';
+
+  # Configure efm-langserver to use markdownlint-cli
+  xdg.configFile."efm-langserver/config.yaml".text = ''
+    version: 2
+    #log-file: "/Users/some-user/.config/efm-langserver/log.log"
+    #log-level: 2
+
+    languages:
+     markdown:
+       - lint-command: "markdownlint --disable MD013 -s"
+         lint-stdin: true
+         lint-after-open: true
+         lint-on-save: true
+         lint-formats:
+           - "%f:%l %m"
+           - "%f:%l:%c %m"
+           - "%f: %l: %m"
   '';
 }
