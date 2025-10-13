974454345e303c0473c2646d00dd27510fd9fda5
willswats <william@williamwatson.dev>
2025-10-13 16:25:10 +0100

    feat: manually define lazygit theme
    catppuccin one was causing issues at times, e.g. kitty launch

--- modules/development/lazygit/home.nix
+++ modules/development/lazygit/home.nix
@@ -1,9 +1,8 @@
-{ pkgs, ... }:
+{ pkgs, globals, ... }:
 
 {
   home.packages = with pkgs; [ lazygit ];
 
-  catppuccin.lazygit.enable = true;
   programs.lazygit = {
     enable = true;
     settings = {
@@ -11,6 +10,32 @@
         disableForcePushing = true; # To ensure that a force push is not fat fingered
         overrideGpg = true; # To not get booted to shell on each commit
       };
+      gui.theme =
+        let
+          red = "#${globals.colours.red}";
+          yellow = "#${globals.colours.yellow}";
+          blue = "#${globals.colours.blue}";
+          lavender = "#${globals.colours.lavender}";
+          text = "#${globals.colours.text}";
+          subtext0 = "#${globals.colours.subtext0}";
+          surface0 = "#${globals.colours.surface0}";
+          surface1 = "#${globals.colours.surface1}";
+        in
+        {
+          activeBorderColor = [
+            "${blue}"
+            "bold"
+          ];
+          inactiveBorderColor = [ "${subtext0}" ];
+          optionsTextColor = [ "${blue}" ];
+          selectedLineBgColor = [ "${surface0}" ];
+          cherryPickedCommitBgColor = [ "${surface1}" ];
+          cherryPickedCommitFgColor = [ "${blue}" ];
+          unstagedChangesColor = [ "${red}" ];
+          defaultFgColor = [ "${text}" ];
+          searchingActiveBorderColor = [ "${yellow}" ];
+          authorColors = [ "#${lavender}" ];
+        };
     };
   };
 }
