6dd980da301aac8368308adc712a7e6ec575585d
willswats <william@williamwatson.dev>
2025-10-21 21:02:36 +0100

    feat(hx): markdownlint -> rumdl

--- modules/editors/helix/lsp/markdown.nix
+++ modules/editors/helix/lsp/markdown.nix
@@ -10,21 +10,11 @@
           language-servers = [
             "scls"
             "marksman"
+            "rumdl"
             "harper-ls"
-            {
-              name = "efm";
-              only-features = [ "diagnostics" ];
-            }
             "mpls"
           ];
           auto-format = true;
-          formatter = {
-            command = "prettier";
-            args = [
-              "--parser"
-              "markdown"
-            ];
-          };
           soft-wrap.enable = true;
           comment-tokens = [
             "-"
@@ -36,6 +26,10 @@
         }
       ];
       language-server = {
+        rumdl = {
+          command = "rumdl";
+          args = [ "server" "-v" ];
+        };
         harper-ls = {
           command = "harper-ls";
           args = [
@@ -47,9 +41,6 @@
               LongSentences = false;
             };
           };
-        };
-        efm = {
-          command = "efm-langserver";
         };
         mpls = {
           command = "mpls";
@@ -63,35 +54,21 @@
     };
     extraPackages = with pkgs; [
       marksman # MD Language server
+      rumdl # Markdown formatter & linter
       harper # Spell checker
-      efm-langserver # Wrapper lsp for markdownlint-cli
-      markdownlint-cli # Markdown linter
       mpls # Markdown preview lsp
     ];
   };
+
+  xdg.configFile."rumdl/rumdl.toml".text = ''      
+    [MD013]
+    line-length = 80
+    enable-reflow = true
+'';
 
   xdg.configFile."helix/external-snippets.toml".text = lib.mkAfter ''
     [[sources.paths]] 
     scope = [ "markdown" ]  
     path = "snippets/markdown.json"    
-  '';
-
-  # Configure efm-langserver to use markdownlint-cli
-  # https://github.com/helix-editor/helix/discussions/11639#discussioncomment-10572676
-  xdg.configFile."efm-langserver/config.yaml".text = ''
-    version: 2
-    #log-file: "/Users/some-user/.config/efm-langserver/log.log"
-    #log-level: 2
-
-    languages:
-     markdown:
-       - lint-command: "markdownlint --disable MD013 -s"
-         lint-stdin: true
-         lint-after-open: true
-         lint-on-save: true
-         lint-formats:
-           - "%f:%l %m"
-           - "%f:%l:%c %m"
-           - "%f: %l: %m"
   '';
 }
