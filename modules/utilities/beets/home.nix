d7cbf3bf4cd34551a0d45abf2e975723beaa3613
willswats <william@williamwatson.dev>
2025-11-30 11:47:40 +0000

    feat(beets): add musicbrainz plugin

--- modules/utilities/beets/home.nix
+++ modules/utilities/beets/home.nix
@@ -4,7 +4,7 @@
   programs.beets = {
     enable = true;
     settings = {
-      plugins = "missing embedart fetchart";
+      plugins = "musicbrainz missing embedart fetchart";
       directory = "${host.directories.drive}/Entertainment/Music/Albums";
     };
   };
