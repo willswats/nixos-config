1475d6596b6dd0350d26310e02aabdc78775ad24
willswats <william@williamwatson.dev>
2025-11-30 11:09:26 +0000

    feat: beets remove mpdupdate

--- modules/utilities/beets/home.nix
+++ modules/utilities/beets/home.nix
@@ -4,7 +4,7 @@
   programs.beets = {
     enable = true;
     settings = {
-      plugins = "mpdupdate missing embedart fetchart";
+      plugins = "missing embedart fetchart";
       directory = "${host.directories.drive}/Entertainment/Music/Albums";
     };
   };
