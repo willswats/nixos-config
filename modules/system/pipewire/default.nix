a280ad91c0b7a2927d1a8ceba732033df2c68239
willswats <william@williamwatson.dev>
2025-12-26 19:54:35 +0000

    feat: re-enable low latency

--- modules/system/pipewire/default.nix
+++ modules/system/pipewire/default.nix
@@ -11,13 +11,12 @@
     jack.enable = true;
     wireplumber.enable = true;
     # For low latency - keep increasing the quant value until you get no crackles
-    # TODO: uncomment when fixed https://github.com/fufexan/nix-gaming/issues/319
-    # lowLatency = {
-    #   # enable this module
-    #   enable = true;
-    #   # defaults (no need to be set unless modified)
-    #   quantum = 1024;
-    #   rate = 48000;
-    # };
+    lowLatency = {
+      # enable this module
+      enable = true;
+      # defaults (no need to be set unless modified)
+      quantum = 512;
+      rate = 48000;
+    };
   };
 }
