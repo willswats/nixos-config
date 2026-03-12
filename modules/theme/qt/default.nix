3da4b1512b7e77cf6eb96406c91723a9cceb01d0
willswats <william@williamwatson.dev>
2026-03-12 15:48:51 +0000

    feat(qt): setup kvantum theming

--- modules/theme/qt/default.nix
+++ modules/theme/qt/default.nix
@@ -1,6 +1,5 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
-  # Fix QT6 theme (https://github.com/NixOS/nixpkgs/pull/211003#issuecomment-1652277619):
-  environment.variables.QT_PLUGIN_PATH = "${pkgs.qt6Packages.qt6gtk2}/lib/qt-6/plugins";
+  environment.variables.QT_QPA_PLATFORMTHEME = "qt5ct";
 }
--- modules/theme/qt/home.nix
+++ modules/theme/qt/home.nix
@@ -1,12 +1,37 @@
-{ pkgs, ... }:
+{ pkgs, globals, ... }:
+
 
 {
-  qt = {
-    enable = true;
-    platformTheme.name = "gtk";
-    style = {
-      name = "gtk2";
-      package = pkgs.qt6Packages.qt6gtk2;
+  home.packages = with pkgs; [
+    libsForQt5.qt5ct
+    kdePackages.qt6ct
+    kdePackages.qtstyleplugin-kvantum
+  ];
+
+  catppuccin.kvantum.enable = true;
+
+  qt =
+    let
+      qtctSettings = {
+        Appearance = {
+          style = "kvantum-dark";
+          icon_theme = "Papirus-Dark";
+          standard_dialogs = "default";
+        };
+        Fonts = {
+          fixed = "\"${globals.font.name},12\"";
+          general = "\"${globals.font.name},12\"";
+        };
+      };
+    in
+    {
+      enable = true;
+      # Cant set this while using catppuccin.kvantum
+      # https://github.com/catppuccin/nix/issues/275
+      # Instead opted for setting environment variable in default.nix (same outcome)
+      # platformTheme.name = "qtct";
+      style.name = "kvantum";
+      qt5ctSettings = qtctSettings;
+      qt6ctSettings = qtctSettings;
     };
-  };
 }
