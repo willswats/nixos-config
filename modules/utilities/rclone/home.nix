35721d9b18bc234e24fa905581b16df9cec9394a
willswats <william@williamwatson.dev>
2026-03-14 19:28:39 +0000

    chore: comment service

--- modules/utilities/rclone/home.nix
+++ modules/utilities/rclone/home.nix
@@ -3,32 +3,33 @@
 {
   home.packages = with pkgs; [ rclone ];
 
-  systemd.user.services.rclone-bisync =
-    let
-      driveDir = host.directories.drive;
-      rclone = "${pkgs.rclone}/bin/rclone";
-      rclone-bisync-command = pkgs.writeShellScript "rclone-bisync-command.sh" ''
-        ${rclone} bisync filen: ${driveDir} \
-          --create-empty-src-dirs \
-          --compare size,modtime,checksum \
-          --slow-hash-sync-only \
-          --log-level INFO \
-          --log-file "/home/will/.config/rclone/rclone.log" \
-          --filters-file "/home/will/.config/rclone/bisync-filters.txt"
-      '';
-    in
-    {
-      Install = {
-        WantedBy = [ "default.target" ];
-      };
+  # TODO: uncomment and test when resync is finished
+  # systemd.user.services.rclone-bisync =
+  #   let
+  #     driveDir = host.directories.drive;
+  #     rclone = "${pkgs.rclone}/bin/rclone";
+  #     rclone-bisync-command = pkgs.writeShellScript "rclone-bisync-command.sh" ''
+  #       ${rclone} bisync filen: ${driveDir} \
+  #         --create-empty-src-dirs \
+  #         --compare size,modtime,checksum \
+  #         --slow-hash-sync-only \
+  #         --log-level INFO \
+  #         --log-file "/home/will/.config/rclone/rclone.log" \
+  #         --filters-file "/home/will/.config/rclone/bisync-filters.txt"
+  #     '';
+  #   in
+  #   {
+  #     Install = {
+  #       WantedBy = [ "default.target" ];
+  #     };
 
-      Service = {
-        Type = "oneshot";
-        ExecStart = rclone-bisync-command.outPath;
-      };
+  #     Service = {
+  #       Type = "oneshot";
+  #       ExecStart = rclone-bisync-command.outPath;
+  #     };
 
-      Unit = {
-        Description = "rclone bisync";
-      };
-    };
+  #     Unit = {
+  #       Description = "rclone bisync";
+  #     };
+  #   };
 }
