4364b1352894fa02d9ec93d44337b1fb81a33d38
willswats <william@williamwatson.dev>
2026-04-21 14:46:04 +0100

    feat: add replaygain

--- modules/utilities/beets/home.nix
+++ modules/utilities/beets/home.nix
@@ -1,11 +1,15 @@
-{ host, ... }:
+{ host, pkgs, ... }:
 
 {
   programs.beets = {
     enable = true;
     settings = {
-      plugins = "mpdupdate musicbrainz missing embedart fetchart";
+      plugins = "mpdupdate musicbrainz missing embedart fetchart replaygain";
       directory = "${host.directories.drive}/Entertainment/Music/Albums";
+      replaygain = {
+        backend = "command";
+        command = "${pkgs.mp3gain}/bin/mp3gain";
+      };
     };
   };
 }
