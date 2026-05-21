dfa8a5dc7f64263830f02a9b78b22dc7026d7ca5
willswats <william@williamwatson.dev>
2026-05-21 09:45:25 +0100

    feat: remove google bookmarks

--- modules/utilities/firefox/home.nix
+++ modules/utilities/firefox/home.nix
@@ -228,27 +228,6 @@
             toolbar = true;
             bookmarks = [
               {
-                name = "Google";
-                bookmarks = [
-                  {
-                    name = "Mail";
-                    url = "https://mail.google.com";
-                  }
-                  {
-                    name = "Calendar";
-                    url = "https://calendar.google.com";
-                  }
-                  {
-                    name = "Contacts";
-                    url = "https://contacts.google.com";
-                  }
-                  {
-                    name = "Drive";
-                    url = "https://drive.google.com";
-                  }
-                ];
-              }
-              {
                 name = "Proton";
                 bookmarks = [
                   {
