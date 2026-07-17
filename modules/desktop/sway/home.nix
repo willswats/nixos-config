5a84319625355b3f61b62891aa62ba43b74e28c1
willswats <william@williamwatson.dev>
2026-07-17 18:12:25 +0100

    fix: start music not in terminal

--- modules/desktop/sway/home.nix
+++ modules/desktop/sway/home.nix
@@ -304,7 +304,7 @@
 
           "${mod}+t" = "exec ${kitty} $EDITOR"; # Text editor
           "${mod}+e" = "exec ${kitty} ${yazi}"; # File explorer
-          "${mod}+m" = "exec ${kitty} ${euphonica}"; # Music player
+          "${mod}+m" = "exec ${euphonica}"; # Music player
           "${mod}+s" = "exec ${kitty} ${btm} -b"; # Process monitor
           "${mod}+b" = "exec ${kitty} ${bluetuith}"; # Bluetooth manager
           "${mod}+n" = "exec ${kitty} ${nyaa}"; # Nyaa
