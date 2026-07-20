72a243f23d271f7487c2496ff340873a879a5172
willswats <william@williamwatson.dev>
2026-07-20 12:56:47 +0100

    feat: add lastgenre

--- modules/utilities/beets/home.nix
+++ modules/utilities/beets/home.nix
@@ -4,11 +4,16 @@
   programs.beets = {
     enable = true;
     settings = {
-      plugins = "mpdupdate musicbrainz missing embedart fetchart replaygain";
+      plugins = "mpdupdate musicbrainz missing embedart fetchart replaygain lastgenre";
       directory = "${host.directories.drive}/Entertainment/Music/Albums";
       replaygain = {
         backend = "command";
         command = "${pkgs.mp3gain}/bin/mp3gain";
+      };
+      lastgenre = {
+        source = "album";
+        count = 3;
+        min_weight = 2;
       };
     };
   };
