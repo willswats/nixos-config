c04939a9428bc990900f54416640232c392a82fa
willswats <william@williamwatson.dev>
2026-08-09 14:01:11 +0100

    chore: update flake

--- modules/utilities/iloader/default.nix
+++ modules/utilities/iloader/default.nix
@@ -1,11 +1,11 @@
 { inputs, pkgs, ... }:
 
 {
-  services.usbmuxd.enable = true;
+  # services.usbmuxd.enable = true;
 
   environment.systemPackages = [
-    inputs.iloader.packages.${pkgs.stdenv.hostPlatform.system}.default
-    pkgs.libimobiledevice
-    pkgs.ifuse
+    # inputs.iloader.packages.${pkgs.stdenv.hostPlatform.system}.default
+    # pkgs.libimobiledevice
+    # pkgs.ifuse
   ];
 }
